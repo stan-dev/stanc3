1850b590a43909517118eeeafa64cdfd8ab82fa9
Sean Talts <sean.talts@gmail.com>
2018-10-02 14:29:01 -0400

    Plans for generating function signatures from Stan 2

---
+++ src/std_lib.rs
@@ -0,0 +1,10 @@
+// In this file, we'd like to keep track of:
+// 1. Function signatures available from the Stan Math library,
+// 2. Functions and other globally available functionality we'd like to add in Rust,
+// 3. Functions we'd like to add with Stan code and make generally available.
+
+// To keep track of what the Math library has available, we'll likely
+// need to import the Stan repo function signatures here as a
+// submodule or some other reference to Stan develop.
+// We'll probably need both the functions.txt output from the manual(?)
+// as well as the function_signatures.h programmatic definitions.
