a80aa6838c067c85e28403c0a37e53beb5ed1fe6
Sean Talts <sean.talts@gmail.com>
2018-10-26 19:45:10 -0400

    Get rid of some cruft; AST will be replaced with Matthijs soon

--- ocaml/lib/Ast.ml
+++ ocaml/lib/Ast.ml
@@ -10,43 +10,6 @@ type expr = Var of string
           | ExprList of expr list
 [@@deriving sexp, hash]
 
-type typePrim = TBool | TReal | TInt
-              | TArray of typePrim * expr list
-              | TVector of expr
-              | TMatrix of expr * expr
-              | TUnit
-
-type typeLevel = LevelVar of string
-               | Data | Model | GenQuant
-               | Lub of typeLevel list | Glb of typeLevel list
-
-type fullType = typePrim
-
-type binOp = Add | Sub | Mul | Div | Mod | CoeffMul
-           | And | Or | Lte | Gte | Neq | Eq | Lt | Gt
-
-type statement = Block of statement list
-               | DistAs of string * expr list
-               | Assign of binOp * string * expr
-               | VarDecl of fullType * string
-               | FCall of string * expr list
-               | If of expr * statement * statement
-               | While of expr * statement
-               | For of string * expr * expr * statement
-               | Break | Continue | Unit
-
-type arg = fullType * string
-
-type funDef = FunExpr of string * arg list * statement * expr
-            | FunVoid of string * arg list * statement * unit
-
-(*
-type stanProg = Prog of funDef list * statement * statement
-              | None
-*)
-
 type stanProg = Prog of expr
               | None
 [@@deriving sexp]
-
-let next() = "0"
