45f2661bfd3c955157b15b3578c5905e6fd80760
Matthijs Vákár <matthijsvakar@gmail.com>
2018-10-29 13:37:19 -0400

    working on symbol table

--- src/stan/symbol_table.ml
+++ src/stan/symbol_table.ml
@@ -4,8 +4,8 @@ module type SYMBOL =
   sig 
   type 'a table
   val initialize : unit -> 'a table
-  val enter : 'a table * string * 'a -> unit  (*
-  val look : 'a table * string -> 'a option
+  val enter : 'a table * string * 'a -> unit  
+  val look : 'a table * string -> 'a option(*
   val begin_scope : 'a table -> unit
   val end_scope : 'a table -> unit *)
 
@@ -13,12 +13,11 @@ end
 
 module Symbol : SYMBOL =
 struct 
-  type 'a table = ((string, 'a) Hashtbl.t) * (('a list) ref)
+  type 'a table = ((string, 'a) Hashtbl.t) * ((string list) ref)
   let initialize _ = (Hashtbl.create 123456, ref []) 
-  let enter (tab, str, ty) =  Hashtbl.add (fst tab) str ty
-  (*
-  val look : a table * string -> a option
-
+  let enter (tab, str, ty) =  Hashtbl.add (fst tab) str ty ; (snd tab) := str :: !(snd tab)
+  let look (tab, str) = Hashtbl.find_opt (fst tab) str
+(*
   val begin_scope : a table -> unit
   val end_scope : a table -> unit *)
 end
