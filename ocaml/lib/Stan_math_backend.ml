16d2f63aa4a3c50ee3ca84f58a5a1c963f205488
Sean Talts <sean.talts@gmail.com>
2018-10-29 17:19:14 -0400

    Illustrate handling special forms with limited MIR

--- ocaml/lib/Stan_math_backend.ml
+++ ocaml/lib/Stan_math_backend.ml
@@ -11,10 +11,23 @@ let rec translate_fn_names = function
       List.map ~f:translate_fn_names args)
   | x -> x
 
+exception ShouldNeverHappen
+
 let rec emit_cpp = function
   | FnApp(f, args) ->
     let args = List.map ~f:emit_cpp args in
-    String.concat [f; "("; (String.concat ~sep:", " args); ")"]
+    begin match f with
+      | "if" | "for" | "while" ->
+        begin match args with
+          | fst :: snd :: thrd ->
+            (sprintf "%s (%s) {\n  %s;\n}%s"
+               f fst snd (List.fold ~init:""
+                            ~f:(fun _ e -> sprintf " else {\n  %s;\n}\n" e)
+                            thrd))
+          | _ -> raise ShouldNeverHappen
+        end
+      | f -> String.concat [f; "("; (String.concat ~sep:", " args); ")"]
+    end
   | Var(v) -> v
   | Lit(Str, s) -> String.concat ["\""; s; "\""]
   | Lit(_, v) -> v
@@ -30,3 +43,21 @@ let%expect_test "emit" =
   let e = FnApp("+", [Lit(Int, "1"); Lit(Real, "3.2")]) in
   emit e |> print_endline;
   [%expect {| add(1, 3.2) |}]
+
+let%expect_test "emitif" =
+  let e = FnApp("if", [Lit(Int, "1"); Lit(Real, "3.2")]) in
+  emit e |> print_endline;
+  [%expect {|
+    if (1) {
+      3.2;
+    } |}]
+
+let%expect_test "emitifelse" =
+  let e = FnApp("if", [Lit(Int, "1"); Lit(Real, "3.2"); Lit(Real, "44.3")]) in
+  emit e |> print_endline;
+  [%expect {|
+    if (1) {
+      3.2;
+    } else {
+      44.3;
+    } |}]
