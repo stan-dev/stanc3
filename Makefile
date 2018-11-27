71bc1b757d585aed0d3575d35dfc8188f31a7605
Sean Talts <sean.talts@gmail.com>
2018-11-27 17:44:45 -0500

    Add pretty printer test

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 all:
 	dune build stanc.exe
 
+test:
+	dune runtest
+
+format:
+	dune build @fmt
+
 cross:
 	dune build stanc.exe -x windows
 
--- test/generate-cram-tests-examples-good.sh
+++
@@ -1,6 +0,0 @@
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )" ;
-for filename in $(find  $dir/examples-good -name '*.stan' -printf "%P\n"); do
-  changepath=$(echo $filename | sed -e 's/[^\/]*/../g')
-  changepath=${changepath::-2}
-  printf "  \$ \$TESTDIR/$changepath/../../_build/default/stanc.exe \"\$TESTDIR/$changepath/$filename\"\n\n" &> $dir/examples-good/${filename}-test.t ; 
-done
