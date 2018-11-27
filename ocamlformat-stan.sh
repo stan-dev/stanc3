b1225c3fc8078c9c093769303bb0c8c63050b8c7
Matthijs Vákár <matthijsvakar@gmail.com>
2018-11-27 08:18:01 -0500

    minor

--- ocamlformat-stan.sh
+++ ocamlformat-stan.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
 shopt -s nullglob
-for filename in *.ml; do
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+for filename in $(find $dir/lib -name '*.ml' -or -name '*.mli'); do
   ocamlformat -i "$filename";
 done;
-for filename in *.mli; do
+for filename in  $dir/*.ml; do
   ocamlformat -i "$filename";
 done;
-cd "lib";
-for filename in *.ml; do
+for filename in  $dir/*.mli; do
   ocamlformat -i "$filename";
 done;
-for filename in *.mli; do
-  ocamlformat -i "$filename";
-done;
-cd ../..;
 # TODO: Find a way to auto-format the .mll and .mly files as well.
