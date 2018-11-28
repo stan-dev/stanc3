13893c9b19949e8a89a2f5369112b766ea7659d9
Sean Talts <sean.talts@gmail.com>
2018-11-28 16:20:57 -0500

    Docker

--- docker/install_stanc_deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# The following looks at what packages we're missing to build stanc.exe
-# and tries to install them with opam.
-install_missing=$(dune external-lib-deps --missing stanc.exe 2>&1 | tail -n1 | cut -c 12-)
-yes | $install_missing
