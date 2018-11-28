4ec4bb41a483435fe88f00af8fc1a904e837a3a1
Sean Talts <sean.talts@gmail.com>
2018-11-28 17:25:52 -0500

    Many dune files

--- test/integration/run_bin_on_args.ml
+++ test/integration/run_bin_on_args.ml
@@ -8,25 +8,9 @@ let run_capturing_output cmd =
   ignore (Unix.close_process_full (stdout, stdin, stderr));
   String.concat ~sep:"\n" (List.concat out)
 
-let walk_directory_trees dirs select =
-  let rec loop result = function
-    | f :: fs when Sys.is_directory f ->
-        Sys.readdir f |> Array.to_list
-        |> List.map ~f:(Filename.concat f)
-        |> List.append fs |> loop result
-    | f :: fs when select f -> loop (f :: result) fs
-    | _ :: fs -> loop result fs
-    | [] -> result
-  in
-  loop [] dirs
-
 let _ =
   let binary = Sys.argv.(1) in
-  let dir_args = Array.(sub Sys.argv ~pos:2 ~len:(length Sys.argv - 2)) in
-  let is_stanfile s = String.is_suffix s ~suffix:".stan" in
-  let dirs = walk_directory_trees (Array.to_list dir_args) is_stanfile in
-  let dirs = List.to_array dirs in
-  (* list to array and back again is ugly *)
+  let dirs = Array.(sub Sys.argv ~pos:2 ~len:(length Sys.argv - 2)) in
   Array.stable_sort ~compare:String.compare dirs ;
   Array.iter dirs ~f:(fun arg ->
       let cmd = binary ^ " " ^ arg in
