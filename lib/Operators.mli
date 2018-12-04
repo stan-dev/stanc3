3db67eb3f52ea52b1802dfe6d7bd13ae61bbf014
Sean Talts <sean.talts@gmail.com>
2018-12-04 14:22:41 -0500

    XXX never merge this, jst for a PR

--- lib/Operators.mli
+++
@@ -1,9 +0,0 @@
-(* -- Helpers for treatment of operators -- *)
-open Ast
-
-val get_operator_return_type_opt :
-  string -> (originblock * unsizedtype) list -> returntype option
-(** Get an optional return type for an operator, given its name and argument types. *)
-
-val pretty_print_all_operator_signatures : string -> string
-(** Print all the signatures of a stan math operator, for the purposes of error messages. *)
--- lib/Parse.mli
+++
@@ -1,7 +0,0 @@
-(** Some complicated stuff to get the custom syntax errors out of Menhir's Incremental
-    API *)
-
-val parse_file :
-     (Lexing.position -> Ast.untyped_program Parser.MenhirInterpreter.checkpoint)
-  -> string
-  -> Ast.untyped_program
