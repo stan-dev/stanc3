1db5ea08382fde84c5fae7ba1c8199fe7e33fb1f
Matthijs Vákár <matthijsvakar@gmail.com>
2018-12-11 15:11:59 -0500

    fixing docker

--- scripts/install_dev_deps.sh
+++ scripts/install_dev_deps.sh
@@ -2,4 +2,4 @@
 # Merlin, utop, ocp-indent, ocamlformat, and patdiff are all for developer assistance
 # Menhir is our parsing library and annoyingly its module name does not match
 # its library name, so we install it manually here.
-opam install -y merlin utop ocp-indent patdiff ocamlformat menhir
+opam install -y merlin utop ocp-indent patdiff ocamlformat menhir ppx_deriving
