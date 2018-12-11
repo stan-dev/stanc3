c1bf863a971d2e8f93e4aa51ddc4e63547788ca7
Sean Talts <sean.talts@gmail.com>
2018-12-11 13:10:37 -0500

    Use ppx_deriving.map instead of writing our own

--- lib/Mir.ml
+++ lib/Mir.ml
@@ -21,9 +21,10 @@ and expr =
   | Indexed of expr * expr list
   (* Different type constructor for indexing by an int array *)
   | MultiIndexed of expr * expr list
+[@@deriving sexp, hash, map]
 
 (* Encode both sized and unsized this way *)
-and stantype =
+type stantype =
   | SInt
   | SReal
   | SArray of expr option * stantype
@@ -65,41 +66,7 @@ and 's statement =
   | For of {loopvar: expr; lower: expr; upper: expr; body: 's}
   | Block of 's list
   | Decl of vardecl * expr option
-[@@deriving sexp, hash]
-
-let map_expr expr_f e =
-  let map_expr_list = List.map ~f:expr_f in
-  match e with
-  | FnApp (n, args) -> FnApp (n, map_expr_list args)
-  | Cond (e1, cond_op, e2) -> Cond (expr_f e1, cond_op, expr_f e2)
-  | ArrayLiteral es -> ArrayLiteral (map_expr_list es)
-  | Indexed (lhs, indices) -> Indexed (expr_f lhs, map_expr_list indices)
-  | e -> e
-
-let map_statement statement_f expr_f s =
-  let map_expr_list = List.map ~f:expr_f in
-  match s with
-  | Assignment a ->
-      Assignment {a with indices= map_expr_list a.indices; rhs= expr_f a.rhs}
-  | NRFnApp (n, args) -> NRFnApp (n, map_expr_list args)
-  | Return e -> Return (expr_f e)
-  | IfElse (cond, ifbranch, elsebranch) ->
-      IfElse
-        ( expr_f cond
-        , statement_f ifbranch
-        , Option.map ~f:statement_f elsebranch )
-  | While (cond, body) -> While (expr_f cond, statement_f body)
-  | For {loopvar; lower; upper; body} ->
-      For
-        { loopvar= expr_f loopvar
-        ; lower= expr_f lower
-        ; upper= expr_f upper
-        ; body= statement_f body }
-  | Block statements -> Block (List.map ~f:statement_f statements)
-  | Decl (d, rhs) -> Decl (d, Option.map ~f:expr_f rhs)
-  | Break -> Break
-  | Continue -> Continue
-  | Skip -> Skip
+[@@deriving sexp, hash, map]
 
 type 's udf_defn =
   { returntype: stantype option
@@ -117,16 +84,6 @@ and 's prog =
   ; tparam: 's
   ; prog_name: string
   ; prog_path: string }
-[@@deriving sexp, hash]
+[@@deriving sexp, hash, map]
 
 type stmt_loc = {sloc: loc; stmt: stmt_loc statement}
-
-let map_udf_defn sf ef udf = {udf with body= map_statement sf ef udf.body}
-
-let map_prog sf ef p =
-  { p with
-    functions= List.map ~f:(map_udf_defn sf ef) p.functions
-  ; model= map_statement sf ef p.model
-  ; gq= map_statement sf ef p.gq
-  ; tdata= map_statement sf ef p.tdata
-  ; tparam= map_statement sf ef p.tparam }
