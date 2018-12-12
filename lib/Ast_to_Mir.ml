6eabb0fbe9f4c73a64fbed0be7d1c5833b184344
Sean Talts <sean.talts@gmail.com>
2018-12-12 15:25:20 -0500

    Add pre-commit hook

--- lib/Ast_to_Mir.ml
+++ lib/Ast_to_Mir.ml
@@ -1,15 +1,16 @@
 (*
 open Mir
+open Core_kernel
 
 let rec ast_to_mir_expr (Ast.TypedExpr (e, meta)) =
+  let raise_expr msg e = raise_s [%message msg (e: Ast.typed_expression Ast.expression)]in
   match e with
-  | Ast.TernaryOp (lhs, op, rhs) ->
-    Cond (ast_to_mir_expr lhs, (match op with
-        | Ast.TypedExpr (e, meta') ->
-          (match e with
-           | Ast.InfixOp (_, _, _) -> (??)
+  | Ast.TernaryOp (Ast.TypedExpr (e', meta'), ifb, elseb) ->
+    let op = (match e' with
+        | Ast.InfixOp (lhs, op, rhs) ->
+          let
+      | _ -> raise_expr "Expected conditional infix operator, got: " e
       )
-      , ast_to_mir_expr rhs)
   | Ast.InfixOp (_, _, _) -> (??)
   | Ast.PrefixOp (_, _) -> (??)
   | Ast.PostfixOp (_, _) -> (??)
@@ -17,11 +18,12 @@ let rec ast_to_mir_expr (Ast.TypedExpr (e, meta)) =
   | Ast.IntNumeral _ -> (??)
   | Ast.RealNumeral _ -> (??)
   | Ast.FunApp (_, _) -> (??)
-  | Ast.CondFunApp (_, _) -> (??)
+  | Ast.CondDistApp (_, _) -> (??)
   | Ast.GetLP -> (??)
   | Ast.GetTarget -> (??)
   | Ast.ArrayExpr _ -> (??)
   | Ast.RowVectorExpr _ -> (??)
   | Ast.Paren _ -> (??)
   | Ast.Indexed (_, _) -> (??)
+
 *)
