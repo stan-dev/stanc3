08d42b3a8a0ab59b354ad27e5d63da1c88b9ab13
Matthijs Vákár <matthijsvakar@gmail.com>
2018-12-13 09:31:04 -0500

    Fixing error messages a bit

--- lib/Errors.ml
+++ lib/Errors.ml
@@ -21,8 +21,8 @@ exception FatalError of string
 let position {Lexing.pos_fname; pos_lnum; pos_cnum; pos_bol} =
   let file = pos_fname in
   let line = pos_lnum in
-  let character = pos_cnum - pos_bol in
-  (file, line, character)
+  let column = pos_cnum - pos_bol in
+  (file, line, column)
 
 let error_context file line column =
   try
@@ -61,43 +61,42 @@ let error_context file line column =
 (** A syntax error message used when handling a SyntaxError *)
 let report_syntax_error = function
   | Parsing (message, start_pos, end_pos) -> (
-      let file, start_line, start_character = position start_pos in
-      let _, curr_line, curr_character = position end_pos in
+      let file, start_line, start_column = position start_pos in
+      let _, curr_line, curr_column = position end_pos in
       let open Printf in
       let lines =
         if curr_line = start_line then sprintf "line %d" curr_line
         else sprintf "lines %d-%d" start_line curr_line
       in
-      let characters =
+      let columns =
         if curr_line = start_line then
-          sprintf "characters %d-%d" start_character curr_character
-        else sprintf "character %d" start_character
+          sprintf "columns %d-%d" start_column curr_column
+        else sprintf "column %d" start_column
       in
       Printf.eprintf
         "\nSyntax error at file \"%s\", %s, %s, parsing error:\n%!" file lines
-        characters ;
-      ( match error_context file curr_line curr_character with
+        columns ;
+      ( match error_context file curr_line curr_column with
       | None -> ()
       | Some line -> Printf.eprintf "%s\n" line ) ;
       match message with
       | None -> Printf.eprintf "\n"
       | Some error_message -> prerr_endline error_message )
   | Lexing (invalid_input, err_pos) ->
-      let file, line, character = position err_pos in
+      let file, line, column = position err_pos in
       Printf.eprintf
-        "\nSyntax error at file \"%s\", line %d, character %d, lexing error:\n"
-        file line (character - 1) ;
-      ( match error_context file line (character - 1) with
+        "\nSyntax error at file \"%s\", line %d, column %d, lexing error:\n"
+        file line (column - 1) ;
+      ( match error_context file line (column - 1) with
       | None -> ()
       | Some line -> Printf.eprintf "%s\n" line ) ;
       Printf.eprintf "Invalid input %S\n%!\n" invalid_input
   | Includes (msg, err_pos) ->
-      let file, line, character = position err_pos in
+      let file, line, column = position err_pos in
       Printf.eprintf
-        "\n\
-         Syntax error at file \"%s\", line %d, character %d, includes error:\n"
-        file line character ;
-      ( match error_context file line character with
+        "\nSyntax error at file \"%s\", line %d, column %d, includes error:\n"
+        file line column ;
+      ( match error_context file line column with
       | None -> ()
       | Some line -> Printf.eprintf "%s\n" line ) ;
       Printf.eprintf "%s\n" msg
@@ -112,11 +111,11 @@ let print_location loc ppf =
       let begin_line = begin_pos.Lexing.pos_lnum in
       let filename = begin_pos.Lexing.pos_fname in
       if String.length filename != 0 then
-        Format.fprintf ppf "file \"%s\", line %d, characters %d-%d" filename
+        Format.fprintf ppf "file \"%s\", line %d, columns %d-%d" filename
           begin_line begin_char end_char
       else
-        Format.fprintf ppf "line %d, characters %d-%d" (begin_line - 1)
-          begin_char end_char
+        Format.fprintf ppf "line %d, columns %d-%d" (begin_line - 1) begin_char
+          end_char
 
 (** A semantic error message used when handling a SemanticError *)
 let report_semantic_error (loc, msg) =
