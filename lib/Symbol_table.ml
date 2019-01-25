8cb207154a00e2a2b7cd5cacf056ea46122fff1f
Matthijs Vákár <matthijsvakar@gmail.com>
2019-01-25 15:33:07 -0500

    Slow progress on switching to Core_kernel

--- lib/Symbol_table.ml
+++ lib/Symbol_table.ml
@@ -1,5 +1,7 @@
 (** Symbol table to implement var map *)
 
+open Core_kernel
+
 (* TODO: I'm sure this implementation could be made more efficient if that's necessary. There's no need for all the string comparison.
 We could just keep track of the count of the entry into the hash table and use that for comparison. *)
 type 'a state =
@@ -11,55 +13,64 @@ type 'a state =
   ; globals: (string, unit) Hashtbl.t }
 
 let initialize () =
-  { table= Hashtbl.create 123456
+  { table= String.Table.create ()
   ; stack= Stack.create ()
   ; scopedepth= ref 0
-  ; readonly= Hashtbl.create 123456
-  ; isunassigned= Hashtbl.create 123456
-  ; globals= Hashtbl.create 123456 }
+  ; readonly= String.Table.create ()
+  ; isunassigned= String.Table.create ()
+  ; globals= String.Table.create () }
 
 (* We just pick some initial size. Hash tables get resized dynamically if necessary, so it doesn't hugely matter. *)
 let enter s str ty =
-  let _ = if !(s.scopedepth) = 0 then Hashtbl.add s.globals str () in
-  Hashtbl.add s.table str ty ; Stack.push str s.stack
+  let _ =
+    if !(s.scopedepth) = 0 then Hashtbl.add s.globals ~key:str ~data:()
+    else `Ok
+  in
+  let _ = Hashtbl.add s.table ~key:str ~data:ty in
+  Stack.push s.stack str
 
 (* recall that OCaml hash tables store a stack of all the values for each key; this would allow us to use shadowing; if we don't want shadowing we can add an extra check here. *)
-let look s str = Hashtbl.find_opt s.table str
+let look s str = Hashtbl.find s.table str
 
 let begin_scope s =
   let _ = s.scopedepth := !(s.scopedepth) + 1 in
-  Stack.push "-sentinel-new-scope-" s.stack
+  Stack.push s.stack "-sentinel-new-scope-"
 
 (* using a string "-sentinel-new-scope-" here that can never be used as an identifier to indicate that new scope is entered *)
 let end_scope s =
   let _ = s.scopedepth := !(s.scopedepth) - 1 in
-  while Stack.top s.stack <> "-sentinel-new-scope-" do
+  while Stack.top_exn s.stack <> "-sentinel-new-scope-" do
     (* we pop the stack down to where we entered the current scope and remove all variables defined since from the var map *)
-    Hashtbl.remove s.table (Stack.top s.stack) ;
-    Hashtbl.remove s.readonly (Stack.top s.stack) ;
-    Hashtbl.remove s.isunassigned (Stack.top s.stack) ;
-    let _ = Stack.pop s.stack in
+    Hashtbl.remove s.table (Stack.top_exn s.stack) ;
+    Hashtbl.remove s.readonly (Stack.top_exn s.stack) ;
+    Hashtbl.remove s.isunassigned (Stack.top_exn s.stack) ;
+    let _ = Stack.pop_exn s.stack in
     ()
   done ;
-  let _ = Stack.pop s.stack in
+  let _ = Stack.pop_exn s.stack in
   ()
 
-let set_read_only s str = Hashtbl.add s.readonly str ()
+let set_read_only s str =
+  let _ = Hashtbl.add s.readonly ~key:str ~data:() in
+  ()
 
 let get_read_only s str =
-  match Hashtbl.find_opt s.readonly str with Some () -> true | _ -> false
+  match Hashtbl.find s.readonly str with Some () -> true | _ -> false
 
 let set_is_assigned s str = Hashtbl.remove s.isunassigned str
 
 let set_is_unassigned s str =
-  if Hashtbl.mem s.isunassigned str then ()
-  else Hashtbl.add s.isunassigned str ()
+  let _ =
+    if Hashtbl.mem s.isunassigned str then `Ok
+    else Hashtbl.add s.isunassigned ~key:str ~data:()
+  in
+  ()
 
 let check_is_unassigned s str = Hashtbl.mem s.isunassigned str
 let check_some_id_is_unassigned s = not (Hashtbl.length s.isunassigned = 0)
 
 let is_global s str =
-  match Hashtbl.find_opt s.globals str with Some _ -> true | _ -> false
+  match Hashtbl.find s.globals str with Some _ -> true | _ -> false
 
 let unsafe_clear_symbol_table s =
   Hashtbl.clear s.table ;
