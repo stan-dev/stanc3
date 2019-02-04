44f79ab424312d3e9c319ae2ac13f4c5428b2daf
Sean Talts <sean.talts@gmail.com>
2019-02-04 16:38:06 -0500

    Separate build from developer deps

--- scripts/install_dev_deps.sh
+++ scripts/install_dev_deps.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Merlin, utop, ocp-indent, ocamlformat, and patdiff are all for developer assistance
-# Menhir is our parsing library and annoyingly its module name does not match
-# its library name, so we install it manually here.
-opam install -y core_kernel menhir ppx_deriving fmt merlin utop ocp-indent patdiff ocamlformat
+opam install -y merlin utop ocp-indent patdiff ocamlformat
