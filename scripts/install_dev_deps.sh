55dcc0dbb49d9bce1bb6e8628bf6e0069946d22d
Sean Talts <sean.talts@gmail.com>
2019-02-04 11:58:36 -0500

    Add dependency on fmt

--- scripts/install_dev_deps.sh
+++ scripts/install_dev_deps.sh
@@ -2,4 +2,4 @@
 # Merlin, utop, ocp-indent, ocamlformat, and patdiff are all for developer assistance
 # Menhir is our parsing library and annoyingly its module name does not match
 # its library name, so we install it manually here.
-opam install -y core_kernel menhir ppx_deriving merlin utop ocp-indent patdiff ocamlformat
+opam install -y core_kernel menhir ppx_deriving fmt merlin utop ocp-indent patdiff ocamlformat
