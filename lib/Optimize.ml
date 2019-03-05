1b43494625bc8e555e3a4d802acfaae43a4adae9
Matthijs Vákár <matthijsvakar@gmail.com>
2019-03-05 09:32:01 -0500

    first stab at some function inlining code

--- lib/Optimize.ml
+++ lib/Optimize.ml
@@ -1,3 +1,43 @@
+(* Code for optimization passes on the MIR *)
+open Core_kernel
+open Mir
+
+let create_function_inline_map {stmt; _} =
+  (* We only add the first definition for each function to the inline map.
+   This will make sure we do not inline recursive functions. *)
+  let f accum {stmt; _} =
+    match stmt with
+    | FunDef {fdname; fdargs; fdbody; _} -> (
+      match Map.add accum ~key:fdname ~data:(fdargs, fdbody) with
+      | `Ok m -> m
+      | `Duplicate -> accum )
+    | _ -> Errors.fatal_error ()
+  in
+  match stmt with
+  | Assignment (_, _)
+   |TargetPE _
+   |NRFunApp (_, _)
+   |Check _ | Break | Continue | Return _ | Skip
+   |IfElse (_, _, _)
+   |While (_, _)
+   |For _ | Block _ | Decl _ | FunDef _ ->
+      Errors.fatal_error ()
+  | SList l -> List.fold l ~init:Map.Poly.empty ~f
+
+let inline_function _ s = s
+
+let function_inlining (mir : stmt_loc prog) =
+  let function_inline_map = create_function_inline_map mir.functionsb in
+  { functionsb= mir.functionsb
+  ; datavars= mir.datavars
+  ; tdatab=
+      (fst mir.tdatab, inline_function function_inline_map (snd mir.tdatab))
+  ; modelb=
+      (fst mir.modelb, inline_function function_inline_map (snd mir.modelb))
+  ; gqb= (fst mir.gqb, inline_function function_inline_map (snd mir.gqb))
+  ; prog_name= mir.prog_name
+  ; prog_path= mir.prog_path }
+
 (* Let's do a simple CSE pass,
 ideally expressed as a visitor with a separate visit() function? *)
 (*
