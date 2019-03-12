35612bd5f16b015fa2ecd86d471cb40edf424368
Ryan Bernstein <ryanbernstein1@gmail.com>
2019-03-12 16:28:45 -0400

    Added inferface for Mir_utils, included more funcs from Dataflow_algorithms

--- lib/Mir_utils.ml
+++ lib/Mir_utils.ml
@@ -2,16 +2,13 @@ open Core_kernel
 open Mir
 open Dataflow_types
 
+(** See interface file *)
 let vexpr_of_expr_exn (ex : expr) : vexpr =
   match ex with
   | Var s -> VVar s
   | _ -> raise (Failure "Non-var expression found, but var expected")
 
-(**
-   The set of variables in an expression, including inside an index
-
-   For use in RHS sets, not LHS assignment sets, except in a target term
-*)
+(** See interface file *)
 let rec expr_var_set (ex : expr) : vexpr Set.Poly.t =
   let union_recur exprs =
     Set.Poly.union_list (List.map exprs ~f:expr_var_set)
@@ -24,7 +21,6 @@ let rec expr_var_set (ex : expr) : vexpr Set.Poly.t =
   | TernaryIf (expr1, expr2, expr3) -> union_recur [expr1; expr2; expr3]
   | Indexed (expr, ix) ->
       Set.Poly.union_list (expr_var_set expr :: List.map ix ~f:index_var_set)
-
 and index_var_set (ix : index) : vexpr Set.Poly.t =
   match ix with
   | All -> Set.Poly.empty
@@ -35,12 +31,16 @@ and index_var_set (ix : index) : vexpr Set.Poly.t =
       Set.Poly.union (expr_var_set expr1) (expr_var_set expr2)
   | MultiIndex expr -> expr_var_set expr
 
-(**
-   The variable being assigned to when `ex` is LHS
-*)
+(** See interface file *)
 let expr_assigned_var (ex : expr) : vexpr =
   match ex with
   | Var s -> VVar s
   | Indexed (Var s, _) -> VVar s
   | _ -> raise (Failure "Unimplemented: analysis of assigning to non-var")
 
+(** See interface file *)
+let rec summation_terms (rhs : expr) : expr list =
+  match rhs with
+  | BinOp (e1, Plus, e2) ->
+      List.append (summation_terms e1) (summation_terms e2)
+  | _ as e -> [e]
---
+++ lib/Mir_utils.mli
@@ -0,0 +1,31 @@
+open Core_kernel
+open Mir
+open Dataflow_types
+
+val vexpr_of_expr_exn : expr -> vexpr
+(**
+   Take a LHS expression from a general expression, throwing an exception if it can't be a
+   LHS expression.
+*)
+
+val expr_var_set : expr -> vexpr Set.Poly.t
+(**
+   The set of variables in an expression, including inside an index.
+
+   For use in RHS sets, not LHS assignment sets, except in a target term.
+*)
+
+val index_var_set : index -> vexpr Set.Poly.t
+(**
+   The set of variables in an index.
+
+   For use in RHS sets, not LHS assignment sets, except in a target term
+*)
+
+val expr_assigned_var : expr -> vexpr
+(**
+   The variable being assigned to when the expression is the LHS
+*)
+
+val summation_terms : expr -> expr list
+(** The list of terms in expression separated by a + *)
