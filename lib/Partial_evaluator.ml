43c4441a5ff2e1c0110d327ba6ecf15eae4f9b34
Matthijs Vakar <matthijsvakar@gmail.com>
2019-03-19 08:52:50 -0400

    cleaning up partial evaluator

--- lib/Partial_evaluator.ml
+++ lib/Partial_evaluator.ml
@@ -3,6 +3,25 @@
 open Core_kernel
 open Mir
 
+let apply_prefix_operator_int (op : string) i =
+  Lit
+    ( Int
+    , Int.to_string
+        ( match op with
+        | "PPlus__" -> i
+        | "PMinus__" -> -i
+        | "PNot__" -> if i = 0 then 1 else 0
+        | _ -> Errors.fatal_error () ) )
+
+let apply_prefix_operator_real (op : string) i =
+  Lit
+    ( Real
+    , Float.to_string
+        ( match op with
+        | "PPlus__" -> i
+        | "PMinus__" -> -.i
+        | _ -> Errors.fatal_error () ) )
+
 let apply_operator_int (op : string) i1 i2 =
   Lit
     ( Int
@@ -186,83 +205,97 @@ let rec eval_expr (e : Mir.expr_typed_located) =
             FunApp ("trace_gen_quad_form", [d; a; b])
         | "trace", [{texpr= FunApp ("quad_form", [a; b]); _}] ->
             FunApp ("trace_quad_form", [a; b])
-        | op, [e1; e2] -> (
-          match (e1, op, e2) with
-          | {texpr= Lit (Int, "1"); _}, "Minus__", {texpr= FunApp ("erf", l); _}
-            ->
+        | op, l -> (
+          match (op, l) with
+          | ( "Minus__"
+            , [{texpr= Lit (Int, "1"); _}; {texpr= FunApp ("erf", l); _}] ) ->
               FunApp ("erfc", l)
-          | ( {texpr= Lit (Int, "1"); _}
-            , "Minus__"
-            , {texpr= FunApp ("erfc", l); _} ) ->
+          | ( "Minus__"
+            , [{texpr= Lit (Int, "1"); _}; {texpr= FunApp ("erfc", l); _}] ) ->
               FunApp ("erf", l)
-          | ( {texpr= FunApp ("exp", l'); _}
-            , "Minus__"
-            , {texpr= Lit (Int, "1"); _} ) ->
+          | ( "Minus__"
+            , [{texpr= FunApp ("exp", l'); _}; {texpr= Lit (Int, "1"); _}] ) ->
               FunApp ("expm1", l')
           (* TODO: can only do below for reals:
     | FunApp (x, "Times__", y), "Plus__", z
     | z, "Plus__", FunApp (x, "Times__",y)-> FunApp ("fma", [x;y;z]) *)
-          | ( {texpr= Lit (Int, "1"); _}
-            , "Minus__"
-            , {texpr= FunApp ("gamma_p", l); _} ) ->
+          | ( "Minus__"
+            , [{texpr= Lit (Int, "1"); _}; {texpr= FunApp ("gamma_p", l); _}] )
+            ->
               FunApp ("gamma_q", l)
-          | ( {texpr= Lit (Int, "1"); _}
-            , "Minus__"
-            , {texpr= FunApp ("gamma_q", l); _} ) ->
+          | ( "Minus__"
+            , [{texpr= Lit (Int, "1"); _}; {texpr= FunApp ("gamma_q", l); _}] )
+            ->
               FunApp ("gamma_p", l)
           (* TODO: can only do below for t reals:       
 | FunApp("matrix_exp", [FunApp(t,"Times__" ,a)]), "Times__", b
 | FunApp("matrix_exp", [FunApp(a,"Times__" ,t)]), "Times__", b-> FunApp("scale_matrix_exp_multiply", [t;a;b]) *)
-          | {texpr= FunApp ("matrix_exp", [a]); _}, "Times__", b ->
+          | "Times__", [{texpr= FunApp ("matrix_exp", [a]); _}; b] ->
               FunApp ("matrix_exp_multiply", [a; b])
           (* TODO: can only do below for reals:  
 | x, "Times__", FunApp("log", [y]) -> FunApp("multiply_log", [x;y]) *)
-          | ( { texpr=
-                  FunApp
-                    ("transpose", [{texpr= FunApp ("diag_matrix", [v]); _}]); _
-              }
-            , "Times__"
-            , { texpr=
-                  FunApp
-                    ("Times__", [a; {texpr= FunApp ("diag_matrix", [w]); _}]); _
-              } )
+          | ( "Times__"
+            , [ { texpr=
+                    FunApp
+                      ("transpose", [{texpr= FunApp ("diag_matrix", [v]); _}]); _
+                }
+              ; { texpr=
+                    FunApp
+                      ("Times__", [a; {texpr= FunApp ("diag_matrix", [w]); _}]); _
+                } ] )
             when v = w ->
               FunApp ("quad_form_diag", [a; v])
-          | ( { texpr=
-                  FunApp
-                    ( "Times__"
-                    , [ { texpr=
-                            FunApp
-                              ( "transpose"
-                              , [{texpr= FunApp ("diag_matrix", [v]); _}] ); _
-                        }
-                      ; a ] ); _ }
-            , "Times__"
-            , {texpr= FunApp ("diag_matrix", [w]); _} )
+          | ( "Times__"
+            , [ { texpr=
+                    FunApp
+                      ( "Times__"
+                      , [ { texpr=
+                              FunApp
+                                ( "transpose"
+                                , [{texpr= FunApp ("diag_matrix", [v]); _}] ); _
+                          }
+                        ; a ] ); _ }
+              ; {texpr= FunApp ("diag_matrix", [w]); _} ] )
             when v = w ->
               FunApp ("quad_form_diag", [a; v])
-          | ( {texpr= FunApp ("transpose", [b]); _}
-            , "Times__"
-            , {texpr= FunApp ("Times__", [a; c]); _} )
+          | ( "Times__"
+            , [ {texpr= FunApp ("transpose", [b]); _}
+              ; {texpr= FunApp ("Times__", [a; c]); _} ] )
             when b = c ->
               FunApp ("quad_form", [a; b])
-          | ( { texpr=
-                  FunApp ("Times__", [{texpr= FunApp ("transpose", [b]); _}; a]); _
-              }
-            , "Times__"
-            , c )
+          | ( "Times__"
+            , [ { texpr=
+                    FunApp
+                      ("Times__", [{texpr= FunApp ("transpose", [b]); _}; a]); _
+                }
+              ; c ] )
             when b = c ->
               FunApp ("quad_form", [a; b])
-          | e1', "Times__", {texpr= FunApp ("diag_matrix", [v]); _} ->
+          | "Times__", [e1'; {texpr= FunApp ("diag_matrix", [v]); _}] ->
               FunApp ("diag_post_multiply", [e1'; v])
-          | {texpr= FunApp ("diag_matrix", [v]); _}, "Times__", e2' ->
+          | "Times__", [{texpr= FunApp ("diag_matrix", [v]); _}; e2'] ->
               FunApp ("diag_pre_multiply", [v; e2'])
               (* Constant folding for operators *)
-          | {texpr= Lit (Int, i1); _}, _, {texpr= Lit (Int, i2); _} ->
-              apply_operator_int op (Int.of_string i1) (Int.of_string i2)
-          | {texpr= Lit (Real, i1); _}, _, {texpr= Lit (Real, i2); _}
-           |{texpr= Lit (Int, i1); _}, _, {texpr= Lit (Real, i2); _}
-           |{texpr= Lit (Real, i1); _}, _, {texpr= Lit (Int, i2); _} -> (
+          | op, [{texpr= Lit (Int, i); _}] -> (
+            match op with
+            | "PPlus_" | "PMinus__" | "PNot__" ->
+                apply_prefix_operator_int op (Int.of_string i)
+            | _ -> FunApp (op, l) )
+          | op, [{texpr= Lit (Real, r); _}] -> (
+            match op with
+            | "PPlus_" | "PMinus__" ->
+                apply_prefix_operator_real op (Float.of_string r)
+            | _ -> FunApp (op, l) )
+          | op, [{texpr= Lit (Int, i1); _}; {texpr= Lit (Int, i2); _}] -> (
+            match op with
+            | "Plus__" | "Minus__" | "Times__" | "Divide__" | "Modulo__"
+             |"Or__" | "And__" | "Equals__" | "NEquals__" | "Less__"
+             |"Leq__" | "Greater__" | "Geq__" ->
+                apply_operator_int op (Int.of_string i1) (Int.of_string i2)
+            | _ -> FunApp (op, l) )
+          | op, [{texpr= Lit (Real, i1); _}; {texpr= Lit (Real, i2); _}]
+           |op, [{texpr= Lit (Int, i1); _}; {texpr= Lit (Real, i2); _}]
+           |op, [{texpr= Lit (Real, i1); _}; {texpr= Lit (Int, i2); _}] -> (
             match op with
             | "Plus__" | "Minus__" | "Times__" | "Divide__" ->
                 apply_arithmetic_operator_real op (Float.of_string i1)
@@ -271,9 +304,8 @@ let rec eval_expr (e : Mir.expr_typed_located) =
              |"Leq__" | "Greater__" | "Geq__" ->
                 apply_logical_operator_real op (Float.of_string i1)
                   (Float.of_string i2)
-            | _ -> FunApp (op, [e1; e2]) )
-          | _ -> FunApp (op, [e1; e2]) )
-        | _, l' -> FunApp (f, l') )
+            | _ -> FunApp (op, l) )
+          | _ -> FunApp (op, l) ) )
       | TernaryIf (e1, e2, e3) -> (
         match (eval_expr e1, eval_expr e2, eval_expr e3) with
         | {texpr= Lit (Int, "0"); _}, _, e3' -> e3'.texpr
