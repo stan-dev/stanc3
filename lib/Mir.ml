41b01fd40cf8af5b2de6b139a5b93b68a5b7a8f8
Michael Thomas <enetsee@pop-os.localdomain>
2019-03-22 10:49:19 +0000

    Refine boxing

--- lib/Mir.ml
+++ lib/Mir.ml
@@ -11,7 +11,11 @@ open Core_kernel
        - mark FnApps as containing print or reject
 *)
 
-type litType = Int | Real | Str [@@deriving sexp, hash]
+type litType =
+  | Int
+  | Real
+  | Str
+[@@deriving sexp, hash]
 
 and 'e index =
   | All
@@ -34,9 +38,15 @@ and 'e expr =
   | Indexed of 'e * 'e index list
 [@@deriving sexp, hash]
 
-let rec pp_expr pp_e ppf = function 
+let pp_builtin_syntax =
+  Fmt.(
+      string
+      |> styled `Yellow
+      )
+
+let rec pp_expr pp_e ppf = function
   | Var varname ->
-      Fmt.string ppf varname 
+      Fmt.string ppf varname
 
   | Lit(Str,str) ->
       Fmt.pf ppf "%S" str
@@ -51,27 +61,29 @@ let rec pp_expr pp_e ppf = function
         |> parens
       ) ppf args
 
-  | TernaryIf(pred,texpr,fexpr) -> 
-    Fmt.pf ppf 
-      {|@[%a@ ?@,%a@,:@ %a@]|}
+  | TernaryIf(pred,texpr,fexpr) ->
+    Fmt.pf ppf
+      {|@[%a@ %a@,%a@,%a@ %a@]|}
       pp_e pred
+      pp_builtin_syntax "?"
       pp_e texpr
+      pp_builtin_syntax ":"
       pp_e fexpr
 
   | Indexed(expr,indices) ->
-    Fmt.pf ppf 
-      {|@[%a[%a]@]|}
-      pp_e expr 
-      Fmt.(list (pp_index pp_e) ~sep:comma) indices
+    Fmt.pf ppf
+      {|@[%a%a@]|}
+      pp_e expr
+      Fmt.(list (pp_index pp_e) ~sep:comma |> brackets) indices
 
-and pp_index pp_e ppf = function 
-  | All -> 
+and pp_index pp_e ppf = function
+  | All ->
       Fmt.char ppf ':'
 
-  | Single index -> 
+  | Single index ->
       pp_e ppf index
 
-  | Upfrom index -> 
+  | Upfrom index ->
       Fmt.pf ppf {|%a:|}
         pp_e index
 
@@ -81,15 +93,14 @@ and pp_index pp_e ppf = function
 
   | Between(lower,upper) ->
       Fmt.pf ppf {|%a:%a|}
-        pp_e lower 
+        pp_e lower
         pp_e upper
 
-  | MultiIndex index -> 
-      (* TODO: I'm not sure what a multi-index is so this formatting probably 
+  | MultiIndex index ->
+      (* TODO: I'm not sure what a multi-index is so this formatting probably
           makes no sense... *)
       Fmt.pf ppf {|~%a|}
         pp_e index
-
 
 type unsizedtype = Ast.unsizedtype [@@deriving sexp, hash]
 
@@ -97,56 +108,72 @@ type autodifftype = Ast.autodifftype [@@deriving sexp, hash]
 
 type returntype = Ast.returntype [@@deriving sexp, hash]
 
-let pp_autodifftype ppf = function 
-  | Ast.DataOnly -> 
-      Fmt.string ppf "dataonly"
+let angle_brackets pp_v ppf v =
+  Fmt.pf ppf "@[<1><%a>@]" pp_v v
 
-  | Ast.AutoDiffable -> 
-      Fmt.string ppf "autodiffable"
+let label str pp_v ppf v =
+  Fmt.pf ppf "%s=%a" str pp_v v
 
-let rec pp_unsizedtype ppf = function 
-  | Ast.UInt -> 
-      Fmt.string ppf "int"
-  | UReal -> 
-      Fmt.string ppf "real"
+let pp_keyword =
+  Fmt.(
+      string
+      |> styled `Blue
+      )
 
-  | UVector -> 
-      Fmt.string ppf "vector"
+let pp_autodifftype ppf = function
+  | Ast.DataOnly ->
+      pp_keyword ppf "data "
 
-  | URowVector -> 
-      Fmt.string ppf "row_vector"
+  | Ast.AutoDiffable ->
+      ()
 
-  | UMatrix -> 
-      Fmt.string ppf "matrix"
+let rec pp_unsizedtype ppf = function
+  | Ast.UInt ->
+      pp_keyword ppf "int"
+  | UReal ->
+      pp_keyword ppf "real"
+
+  | UVector ->
+      pp_keyword ppf "vector"
+
+  | URowVector ->
+      pp_keyword ppf "row_vector"
+
+  | UMatrix ->
+      pp_keyword ppf "matrix"
 
   | UArray ut ->
       (Fmt.brackets pp_unsizedtype) ppf ut
-      
+
   | UFun(argtypes, rt) ->
       Fmt.pf ppf
-        {|%a@,=>@ %a|}
-        Fmt.(list (pair ~sep:comma pp_autodifftype pp_unsizedtype) ~sep:comma) argtypes
+        {|%a => %a|}
+        Fmt.(
+          list
+            (pair ~sep:comma pp_autodifftype pp_unsizedtype)
+            ~sep:comma
+          ) argtypes
         pp_returntype rt
 
 
-  | UMathLibraryFunction -> 
-      Fmt.string ppf "<Stan Math function>"
+  | UMathLibraryFunction ->
+      (angle_brackets Fmt.string) ppf "Stan Math function"
 
-and pp_returntype ppf = function 
+and pp_returntype ppf = function
   | Ast.Void ->
       Fmt.string ppf "void"
 
-  | Ast.ReturnType ut -> 
+  | Ast.ReturnType ut ->
       pp_unsizedtype ppf ut
 
-let no_loc = 
+let no_loc =
   { Ast.filename = ""
   ; line_num = 0
   ; col_num = 0
   ; included_from = None
   }
 
-let no_span = 
+let no_span =
   { Ast.begin_loc = no_loc
   ; end_loc = no_loc
   }
@@ -184,117 +211,137 @@ and ('e, 's) statement =
       ; fdbody: 's }
 [@@deriving sexp, hash]
 
-let pp_fun_arg_decl ppf (autodifftype,name,unsizedtype) = 
-  Fmt.pf ppf 
-    "%a@ %s@ %a"
+let pp_fun_arg_decl ppf (autodifftype,name,unsizedtype) =
+  Fmt.pf ppf
+    "%a%a %s"
     pp_autodifftype autodifftype
-    name
     pp_unsizedtype unsizedtype
+    name
 
-let pp_statement pp_e pp_s ppf = function 
-  | Assignment (assignee,expr) -> 
+
+let rec pp_statement pp_e pp_s ppf = function
+  | Assignment (assignee,expr) ->
       Fmt.pf ppf
-        {|%a@ :=@,%a|}
+        {|@[<h>%a :=@ %a;@]|}
         pp_e assignee
         pp_e expr
 
-  | TargetPE expr -> 
-      Fmt.pf ppf 
-        {|target@ +=@,%a|}
-        pp_e expr 
-
-  | NRFunApp(name,args) -> 
-      Fmt.pf ppf 
-        {|%s[@(<hov>%a@])|}
-        name 
-        Fmt.(list pp_e ~sep:comma) args
-
-  | Check(ident, exprs) -> 
-      (* TODO: I'm not sure what a `Check` statement is so this may not make
-        sense *)
-      Fmt.pf ppf 
-        {|check(%s)@[<hov>{%a@]}|}
-        ident 
-        Fmt.(list pp_e ~sep:comma) exprs
-
-  | Break -> 
-      Fmt.string ppf "break"
-
-  | Continue -> 
-      Fmt.string ppf "continue"
-
-  | Skip -> 
-      Fmt.string ppf "continue"
-
-  | Return (Some expr) -> 
-      Fmt.pf ppf 
-        {|return@ %a|}
+  | TargetPE expr ->
+      Fmt.pf ppf
+        {|@[<h>%a +=@ %a;@]|}
+        pp_keyword "target"
         pp_e expr
 
-  | Return _ -> 
-      Fmt.string ppf "return"
+  | NRFunApp(name,args) ->
+      Fmt.pf ppf
+        {|@[%s%a;@]|}
+        name
+        Fmt.(list pp_e ~sep:comma |> parens) args
 
-  | IfElse(pred,s_true,Some s_false) -> 
-      Fmt.pf ppf 
-        {|if(%a){@[<2>%a@]}@ else @ {@[<2>%a@]}|}
+  | Check(ident, exprs) ->
+      (* TODO: I'm not sure what a `Check` statement is so this may not make
+        sense *)
+      Fmt.pf ppf
+        {|@[check_%s%a;@]|}
+        ident
+        Fmt.(list pp_e ~sep:comma |> parens) exprs
+
+  | Break ->
+      pp_keyword ppf "break;"
+
+  | Continue ->
+      pp_keyword ppf "continue;"
+
+  | Skip ->
+      pp_keyword ppf "skip;"
+
+  | Return (Some expr) ->
+      Fmt.pf ppf
+        {|%a %a;|}
+        pp_keyword "return"
+        pp_e expr
+
+  | Return _ ->
+      pp_keyword ppf  "return;"
+
+  | IfElse(pred,s_true,Some s_false) ->
+      Fmt.pf ppf
+        {|@[<v2>@[%a(%a)@] {@;%a@]@;@[<v2>@[} %a@] {@;%a@]@;}|}
+        pp_builtin_syntax "if"
         pp_e pred
         pp_s s_true
+        pp_builtin_syntax "else"
         pp_s s_false
 
-  | IfElse(pred,s_true,_) -> 
-      Fmt.pf ppf 
-        {|if(%a){@[<2>%a@]}|}
+  | IfElse(pred,s_true,_) ->
+      Fmt.pf ppf
+        {|@[<v2>@[%a(%a)@] {@;%a@]@;}|}
+        pp_builtin_syntax "if"
         pp_e pred
         pp_s s_true
 
-  | While(pred,stmt) -> 
-      Fmt.pf ppf 
-        {|while(%a){@[<2>%a@]}|}
+  | While(pred,stmt) ->
+      Fmt.pf ppf
+        {|@[<v2>@[%a(%a)@] {@;%a@]@;}|}
+        pp_builtin_syntax "while"
         pp_e pred
         pp_s stmt
-  
-  | For {loopvar; lower; upper; body} -> 
-      Fmt.pf ppf 
-        {|for(%s in %a:%a)@ {@[<2>%a@]}|}
-        loopvar
-        pp_e lower 
-        pp_e upper 
-        pp_s body
-  | Block stmts -> 
-      Fmt.pf ppf 
-        {|{@[<2>%a@]}|}
+
+  | For {loopvar; lower; upper; body } ->
+        Fmt.pf ppf
+          {|@[<v2>@[%a(%s in %a:%a)@] {@;%a@]@;}|}
+          pp_builtin_syntax "for"
+          loopvar
+          pp_e lower
+          pp_e upper
+          pp_s body
+
+  | Block stmts ->
+      Fmt.pf ppf
+        {|@[<v>%a@]|}
         Fmt.(list pp_s ~sep:Fmt.cut) stmts
 
-  | SList stmts -> 
-      Fmt.(list pp_s ~sep:Fmt.cut) ppf stmts
+  | SList stmts ->
+      Fmt.(
+        list pp_s ~sep:Fmt.cut
+        |> vbox
+        ) ppf stmts
 
-  | Decl {decl_adtype; decl_id; decl_type} -> 
-      Fmt.pf ppf 
-        {|%a@ %a@ %s|}
+  | Decl {decl_adtype; decl_id; decl_type} ->
+      Fmt.pf ppf
+        {|%a%a %s;|}
         pp_autodifftype decl_adtype
         pp_unsizedtype decl_type
         decl_id
 
-  | FunDef {fdrt; fdname; fdargs; fdbody} -> 
-      match fdrt with 
-      | Some rt -> 
-          Fmt.pf ppf 
-            {|%a@ %s%a@ {@[<2>%a@]}|}
-            pp_unsizedtype rt 
+  | FunDef {fdrt; fdname; fdargs; fdbody} ->
+      match fdrt with
+      | Some rt ->
+          Fmt.pf ppf
+            {|@[<v2>%a %s%a {@ %a@]@ }|}
+            pp_unsizedtype rt
             fdname
             Fmt.(list pp_fun_arg_decl ~sep:comma |> parens) fdargs
             pp_s fdbody
 
-(** A "top var" is a global variable visible to the I/O of Stan.
+      | _ ->
+          Fmt.pf ppf
+            {|@[<v2>%s%a {@ %a@]@ }|}
+            fdname
+            Fmt.(list pp_fun_arg_decl ~sep:comma |> parens) fdargs
+            pp_s fdbody
+
+
+(** A "top var" is a global variable visible to the I/O of Stan
    Local vs. Global vardecls
    There are "local" (i.e. not top-level; not read in or written out anywhere) variable
    declarations that do not allow transformations. These are the only kind allowed in
-   the model block, and any declarations in a Block will also be local.
+   the model block, and any declarations in a Block will also be local
    There are also then top-level ones, which are the only thing you can
    write in both the parameters and data block. The generated quantities block allows both
-   types of variable declarations and, worse, mixes in top-level ones with normal ones.
+   types of variable declarations and, worse, mixes in top-level ones with normal ones
    We'll need to scan the list of declarations for top-level ones and essentially remove them
-   from the block. The AST has an `is_global` flag that also tracks this.
+   from the block. The AST has an `is_global` flag that also tracks this
 *)
 type 'e top_var_decl =
   { tvident : string
@@ -303,24 +350,18 @@ type 'e top_var_decl =
   ; tvloc : Ast.location_span sexp_opaque [@compare.ignore] }
 [@@deriving sexp]
 
-let angle_brackets pp_v ppf v = 
-  Fmt.pf ppf "@[<1><%a>@]" pp_v v
-
-let label str pp_v ppf v = 
-  Fmt.pf ppf "%s=%a" str pp_v v
-
-let pp_transformation pp_e ppf = function 
-  | Ast.Identity -> 
+let pp_transformation pp_e ppf = function
+  | Ast.Identity ->
       ()
-  
-  | Lower expr -> 
+
+  | Lower expr ->
       (pp_e |> label "lower" |> angle_brackets) ppf expr
 
-  | Upper expr -> 
+  | Upper expr ->
       (pp_e |> label "upper" |> angle_brackets) ppf expr
 
-  | LowerUpper(lower_expr,upper_expr) -> 
-      (Fmt.(pair ~sep:comma 
+  | LowerUpper(lower_expr,upper_expr) ->
+      (Fmt.(pair ~sep:comma
               (pp_e |> label "lower")
               (pp_e |> label "upper")
           )
@@ -333,96 +374,92 @@ let pp_transformation pp_e ppf = function
       (pp_e |> label "multiplier" |> angle_brackets) ppf expr
 
   | OffsetMultiplier(offset_expr,mult_expr) ->
-      (Fmt.(pair ~sep:comma 
+      (Fmt.(pair ~sep:comma
               (pp_e |> label "offset")
               (pp_e |> label "multiplier")
           )
           |> angle_brackets) ppf (offset_expr,mult_expr)
 
-  | Ordered -> 
-      Fmt.string ppf "<ordered>"
+  | Ordered ->
+      (angle_brackets Fmt.string) ppf "ordered"
 
   | PositiveOrdered ->
-      Fmt.string ppf "<positive_ordered>"
+      (angle_brackets Fmt.string) ppf "positive_ordered"
 
   | Simplex ->
-      Fmt.string ppf "<simplex>"
+      (angle_brackets Fmt.string) ppf "simplex"
 
   | UnitVector ->
-      Fmt.string ppf "<unit_vector>"
+      (angle_brackets Fmt.string) ppf "unit_vector"
 
   | CholeskyCorr ->
-      Fmt.string ppf "<cholesky_factor_corr>"
+      (angle_brackets Fmt.string) ppf "cholesky_factor_corr"
 
-  | CholeskyCov -> 
-      Fmt.string ppf "<cholesky_factor_cov>"
+  | CholeskyCov ->
+      (angle_brackets Fmt.string) ppf "cholesky_factor_cov"
 
-  | Correlation -> 
-      Fmt.string ppf "<corr_matrix>"
+  | Correlation ->
+      (angle_brackets Fmt.string) ppf "corr_matrix"
 
   | Covariance ->
-      Fmt.string ppf "<cov_matrix>"
+      (angle_brackets Fmt.string) ppf "cov_matrix"
 
-let rec pp_sizedtype pp_e ppf (st,trans)= 
+let rec pp_sizedtype pp_e ppf (st,trans)=
   match st with
   | Ast.SInt ->
-      Fmt.pf ppf 
+      Fmt.pf ppf
         {|%s%a|}
         "int"
         (pp_transformation pp_e) trans
 
   | Ast.SReal ->
-      Fmt.pf ppf 
+      Fmt.pf ppf
         {|%s%a|}
         "real"
         (pp_transformation pp_e) trans
 
-  | Ast.SVector expr -> 
-      Fmt.pf ppf 
+  | Ast.SVector expr ->
+      Fmt.pf ppf
         {|vector%a%a|}
         (pp_transformation pp_e) trans
         (Fmt.brackets pp_e) expr
-        
 
-  | Ast.SRowVector expr -> 
-      Fmt.pf ppf 
+  | Ast.SRowVector expr ->
+      Fmt.pf ppf
         {|row_vector%a%a|}
         (pp_transformation pp_e) trans
         (Fmt.brackets pp_e) expr
-        
 
-  | Ast.SMatrix(d1_expr,d2_expr) -> 
-      Fmt.pf ppf 
+  | Ast.SMatrix(d1_expr,d2_expr) ->
+      Fmt.pf ppf
         {|matrix%a%a|}
         (pp_transformation pp_e) trans
         Fmt.(pair ~sep:comma pp_e pp_e |> brackets) (d1_expr,d2_expr)
-        
 
-  | Ast.SArray(st,expr) -> 
-      Fmt.pf ppf 
+  | Ast.SArray(st,expr) ->
+      Fmt.pf ppf
         {|array%a%a|}
         (pp_transformation pp_e) trans
-        Fmt.(pair ~sep:comma 
-              (fun ppf st -> 
+        Fmt.(pair ~sep:comma
+              (fun ppf st ->
                 pp_sizedtype pp_e ppf (st,Ast.Identity)
-              ) pp_e 
+              ) pp_e
               |> brackets
             ) (st,expr)
 
-let pp_top_var_decl pp_e ppf { tvident; tvtype; tvtrans; _ } = 
-  Fmt.pf ppf 
-    {|%a@ %s|}
+let pp_top_var_decl pp_e ppf { tvident; tvtype; tvtrans; _ } =
+  Fmt.pf ppf
+    {|@[<h>%a@ %s;@]|}
     (pp_sizedtype pp_e) (tvtype,tvtrans)
     tvident
 
 type 'e top_var_table = (string, 'e top_var_decl) Map.Poly.t [@@deriving sexp]
 
-let pp_top_var_table pp_e ppf (tbl : 'e top_var_table) = 
+let pp_top_var_table pp_e ppf (tbl : 'e top_var_table) =
   Map.Poly.data tbl
-  |> Fmt.pf ppf 
-    {|@[<v>%a@]|}
-    Fmt.(list ~sep:cut (pp_top_var_decl pp_e))
-      
+  |> Fmt.pf ppf
+      {|@[<v>%a@]|}
+      Fmt.(list ~sep:cut (pp_top_var_decl pp_e))
 
 type ('e, 's) prog =
   { functions_block: 's list
@@ -442,31 +479,65 @@ type ('e, 's) prog =
   ; prog_path : string }
 [@@deriving sexp]
 
-let pp_statement_block label pp_s ppf  = 
-  Fmt.pf ppf 
-    "%s@ @[<v 2>{%a@]}"
-    label 
-    Fmt.(list ~sep:cut pp_s)
+let pp_statement_block label pp_s ppf  stmts =
+  Fmt.pf ppf
+    {|@[<v2>%a {@ %a@]@ }|}
+    pp_keyword label
+    Fmt.(list ~sep:cut pp_s ) stmts;
+  Format.pp_force_newline ppf ()
 
-let pp_decl_block label pp_e ppf =
-  Fmt.pf ppf 
-    "%s@ @[<v 2>{%a@]}"
-    label 
-    (pp_top_var_table pp_e)
+let pp_decl_block label pp_e ppf decls =
+  Fmt.pf ppf
+    {|@[<v2>%a {@ %a@]@ }|}
+    pp_keyword label
+    (pp_top_var_table pp_e ) decls;
+  Format.pp_force_newline ppf ()
 
-let pp_prog pp_e pp_s ppf prog = 
-  Fmt.pf ppf 
-    "%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;"
-    (pp_statement_block "functions" pp_s) prog.functions_block
-    (pp_decl_block "data" pp_e) prog.data_vars
-    (pp_decl_block "transformed_data" pp_e) prog.tdata_vars
-    (pp_statement_block "prepare_data" pp_s) prog.prepare_data
-    (pp_decl_block "parameters" pp_e) prog.params
-    (pp_decl_block "transformed_parameters" pp_e) prog.tparams
-    (pp_statement_block "prepare_parameters" pp_s) prog.prepare_params
-    (pp_statement_block "log_prob" pp_s) prog.log_prob
-    (pp_decl_block "generated_quantities_variables" pp_e) prog.gen_quant_vars
-    (pp_statement_block "generated_quantities" pp_s) prog.generate_quantities
+(* -- Exposed pretty printer -------------------------------------------------*)
+
+let pp_functions_block pp_s ppf {functions_block ; _} =
+  (pp_statement_block "functions" pp_s) ppf functions_block
+
+let pp_data_vars pp_e ppf {data_vars; _} =
+  (pp_decl_block "data_vars" pp_e) ppf data_vars
+
+let pp_tdata_vars pp_e ppf {tdata_vars; _} =
+  (pp_decl_block "transformed_data_vars" pp_e) ppf tdata_vars
+
+let pp_prepare_data pp_s ppf {prepare_data;_} =
+  (pp_statement_block "prepare_data" pp_s) ppf prepare_data
+
+let pp_params pp_e ppf {params; _} =
+  (pp_decl_block "parameters" pp_e) ppf params
+
+let pp_tparams pp_e ppf {tparams; _} =
+  (pp_decl_block "transformed_parameters" pp_e) ppf tparams
+
+let pp_prepare_params pp_s ppf {prepare_params;_} =
+  (pp_statement_block "prepare_parameters" pp_s) ppf prepare_params
+
+let pp_log_prob pp_s ppf {log_prob;_} =
+  (pp_statement_block "log_prob" pp_s) ppf log_prob
+
+let pp_gen_quant_vars pp_e ppf {gen_quant_vars;_} =
+  (pp_decl_block "generated_quantities_vars" pp_e) ppf gen_quant_vars
+
+let pp_generate_quantities pp_s ppf {generate_quantities;_} =
+  (pp_statement_block "log_prob" pp_s) ppf generate_quantities
+
+let pp_prog pp_e pp_s ppf prog =
+  Fmt.pf ppf
+    "@[<v>@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;%a@;@]"
+    (pp_functions_block pp_s) prog
+    (pp_data_vars pp_e) prog
+    (pp_tdata_vars pp_e) prog
+    (pp_prepare_data pp_s) prog
+    (pp_params pp_e) prog
+    (pp_tparams pp_e) prog
+    (pp_prepare_params pp_s) prog
+    (pp_log_prob pp_s) prog
+    (pp_gen_quant_vars pp_e) prog
+    (pp_generate_quantities pp_s) prog
 
 type expr_typed_located =
   { texpr_type: Ast.unsizedtype
@@ -475,7 +546,7 @@ type expr_typed_located =
   ; texpr_adlevel: autodifftype }
 [@@deriving sexp, hash]
 
-let rec pp_texpr_typed_located ppf { texpr; _ }  = 
+let rec pp_texpr_typed_located ppf { texpr; _ }  =
   pp_expr pp_texpr_typed_located ppf texpr
 
 type stmt_loc =
@@ -483,13 +554,13 @@ type stmt_loc =
   ; stmt: (expr_typed_located, stmt_loc) statement }
 [@@deriving sexp, hash]
 
-let rec pp_stmt_loc ppf { stmt; _ }  = 
+let rec pp_stmt_loc ppf { stmt; _ }  =
   pp_statement pp_texpr_typed_located pp_stmt_loc ppf stmt
 
 type typed_prog = (expr_typed_located, stmt_loc) prog [@@deriving sexp]
 
-let pp_typed_prog : typed_prog Fmt.t = 
-  pp_prog pp_texpr_typed_located pp_stmt_loc 
+let pp_typed_prog : typed_prog Fmt.t =
+  pp_prog pp_texpr_typed_located pp_stmt_loc
 
 (* ===================== Some helper functions and values ====================== *)
 
@@ -509,6 +580,6 @@ let internal_expr =
   ; texpr_type= UInt
   ; texpr_adlevel= DataOnly }
 
-let zero = 
+let zero =
   { internal_expr with texpr= Lit (Int, "0")
   ; texpr_type= UInt }
