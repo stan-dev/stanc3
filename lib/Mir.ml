31077f1551fb13a81e874e3af655f6cf1e41de32
Michael Thomas <mthomas180@gmail.com>
2019-03-27 11:23:03 +0000

    Add semicolon after `io_var` declaration

--- lib/Mir.ml
+++ lib/Mir.ml
@@ -236,7 +236,7 @@ let pp_io_block ppf = function
 type 'e io_var = string * ('e sizedtype * io_block) [@@deriving sexp]
 
 let pp_io_var pp_e ppf (name, (sized_ty, io_block)) =
-  Fmt.pf ppf "@[<h>%a %s %a@]" (pp_sizedtype pp_e) (sized_ty, Ast.Identity)
+  Fmt.pf ppf "@[<h>%a %s %a;@]" (pp_sizedtype pp_e) (sized_ty, Ast.Identity)
     name
     (angle_brackets @@ angle_brackets pp_io_block)
     io_block
