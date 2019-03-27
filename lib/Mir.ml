8310d03cf73cb0db1fc09a2f236a7cf88399b3b1
Matthijs Vakar <matthijsvakar@gmail.com>
2019-03-27 11:21:14 -0400

    simplifying definition of ExprSet and ExprMap

--- lib/Mir.ml
+++ lib/Mir.ml
@@ -242,11 +242,7 @@ let zero = {internal_expr with texpr= Lit (Int, "0"); texpr_type= UInt}
 
 (**  A custom comparator which ignores locations on expressions *)
 module ExprComparator = struct
-  type t = expr_typed_located
-
-  let sexp_of_t = sexp_of_expr_typed_located
-  let t_of_sexp = expr_typed_located_of_sexp
-  let compare = compare_expr_typed_located
+  type t = expr_typed_located [@@deriving sexp, compare]
 end
 
 (**  A module for sets of expressions which ignore their locations *)
