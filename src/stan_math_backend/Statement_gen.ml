b28fd0038aad8ec0d9187f703ba3d6a5b1438793
Matthijs Vakar <matthijsvakar@gmail.com>
2019-06-14 12:09:41 -0400

    Tidying a bit

--- src/stan_math_backend/Statement_gen.ml
+++ src/stan_math_backend/Statement_gen.ml
@@ -78,88 +78,93 @@ let trans_math_fn fname =
 let rec pp_statement (ppf : Format.formatter)
     ({stmt; smeta} : (mtype_loc_ad, 'a) stmt_with) =
   let pp_stmt_list = list ~sep:cut pp_statement in
-  let _ = pf ppf "{@;<1 2>@[<v>" in
-  let _ = Locations.pp_smeta ppf smeta in
-  (match stmt with
-  | Assignment (lhs, {expr= FunApp (CompilerInternal, f, _) as expr; emeta})
-    when internal_fn_of_string f = Some FnReadData ->
-      let with_vestigial_idx =
-        {expr= Indexed ({expr; emeta}, [Single loop_bottom]); emeta}
-      in
-      pp_statement ppf {stmt= Assignment (lhs, with_vestigial_idx); smeta}
-      (* XXX In stan2 this often generates:
+  let pp_statement_body ppf stmt =
+    match stmt with
+    | Assignment (lhs, {expr= FunApp (CompilerInternal, f, _) as expr; emeta})
+      when internal_fn_of_string f = Some FnReadData ->
+        let with_vestigial_idx =
+          {expr= Indexed ({expr; emeta}, [Single loop_bottom]); emeta}
+        in
+        pp_statement ppf {stmt= Assignment (lhs, with_vestigial_idx); smeta}
+        (* XXX In stan2 this often generates:
                 stan::model::assign(theta,
                             stan::model::cons_list(stan::model::index_uni(j), stan::model::nil_index_list()),
                             (mu + (tau * get_base1(theta_tilde,j,"theta_tilde",1))),
                             "assigning variable theta");
             }
         *)
-  | Assignment (lhs, {expr= Lit (Str, s); _}) ->
-      pf ppf "%a = %S;" pp_indexed_simple lhs s
-  | Assignment (lhs, {expr= Lit (_, s); _}) ->
-      pf ppf "%a = %s;" pp_indexed_simple lhs s
-  | Assignment (lhs, ({expr= FunApp (CompilerInternal, f, _); _} as rhs))
-    when internal_fn_of_string f = Some FnMakeArray ->
-      pf ppf "%a = @[<hov>%a;@]" pp_indexed_simple lhs pp_expr rhs
-  | Assignment ((assignee, idcs), rhs) ->
-      let rec maybe_deep_copy vident = function
-        | {expr= Var v; _} as e when v = vident ->
-            { e with
-              expr= FunApp (CompilerInternal, "stan::model::deep_copy", [e]) }
-        | {expr; emeta} -> {expr= map_expr (maybe_deep_copy vident) expr; emeta}
-      in
-      let rhs =
-        match rhs.expr with
-        | FunApp (CompilerInternal, f, _)
-          when f = string_of_internal_fn FnConstrain
-               || f = string_of_internal_fn FnUnconstrain ->
-            rhs
-        | _ -> maybe_deep_copy assignee rhs
-      in
-      pf ppf "assign(@[<hov>%s, %a, %a, %S@]);" assignee pp_indexes idcs
-        pp_expr rhs
-        (strf "assigning variable %a" pp_indexed_simple (assignee, idcs))
-  | TargetPE e -> pf ppf "lp_accum__.add(%a);" pp_expr e
-  | NRFunApp (CompilerInternal, fname, {expr= Lit (Str, check_name); _} :: args)
-    when fname = string_of_internal_fn FnCheck ->
-      let args = {expr= Var "function__"; emeta= internal_meta} :: args in
-      pp_statement ppf
-        {stmt= NRFunApp (CompilerInternal, "check_" ^ check_name, args); smeta}
-  | NRFunApp (CompilerInternal, fname, [var])
-    when fname = string_of_internal_fn FnWriteParam ->
-      pf ppf "vars__.push_back(@[<hov>%a@]);" pp_expr var
-  | NRFunApp (CompilerInternal, fname, args) ->
-      let fname, extra_args = trans_math_fn fname in
-      pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr)
-        (extra_args @ args)
-  | NRFunApp (StanLib, fname, args) ->
-      pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr) args
-  | NRFunApp (UserDefined, fname, args) ->
-      pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr) args
-  | Break -> string ppf "break;"
-  | Continue -> string ppf "continue;"
-  | Return e -> pf ppf "return %a;" (option pp_expr) e
-  | Skip -> ()
-  | IfElse (cond, ifbranch, elsebranch) ->
-      let pp_else ppf x = pf ppf "else %a" pp_statement x in
-      pf ppf "if (@[<hov>%a@]) %a %a" pp_expr cond pp_block_s ifbranch
-        (option pp_else) elsebranch
-  | While (cond, body) ->
-      pf ppf "while (@[<hov>%a@]) %a" pp_expr cond pp_block_s body
-  | For
-      { body=
-          {stmt= Assignment (_, {expr= FunApp (CompilerInternal, f, _); _}); _}
-          as body; _ }
-    when internal_fn_of_string f = Some FnReadParam ->
-      pp_statement ppf body
-      (* Skip For loop part, just emit body due to the way FnReadParam emits *)
-  | For {loopvar; lower; upper; body} ->
-      pp_for_loop ppf (loopvar, lower, upper, pp_statement, body)
-  | Block ls -> pp_block ppf (pp_stmt_list, ls)
-  | SList ls -> pp_stmt_list ppf ls
-  | Decl {decl_adtype; decl_id; decl_type} ->
-      pp_possibly_sized_decl ppf (decl_id, decl_type, decl_adtype)) ;
-       pf ppf "@]@,}" 
+    | Assignment (lhs, {expr= Lit (Str, s); _}) ->
+        pf ppf "%a = %S;" pp_indexed_simple lhs s
+    | Assignment (lhs, {expr= Lit (_, s); _}) ->
+        pf ppf "%a = %s;" pp_indexed_simple lhs s
+    | Assignment (lhs, ({expr= FunApp (CompilerInternal, f, _); _} as rhs))
+      when internal_fn_of_string f = Some FnMakeArray ->
+        pf ppf "%a = @[<hov>%a;@]" pp_indexed_simple lhs pp_expr rhs
+    | Assignment ((assignee, idcs), rhs) ->
+        let rec maybe_deep_copy vident = function
+          | {expr= Var v; _} as e when v = vident ->
+              { e with
+                expr= FunApp (CompilerInternal, "stan::model::deep_copy", [e])
+              }
+          | {expr; emeta} ->
+              {expr= map_expr (maybe_deep_copy vident) expr; emeta}
+        in
+        let rhs =
+          match rhs.expr with
+          | FunApp (CompilerInternal, f, _)
+            when f = string_of_internal_fn FnConstrain
+                 || f = string_of_internal_fn FnUnconstrain ->
+              rhs
+          | _ -> maybe_deep_copy assignee rhs
+        in
+        pf ppf "assign(@[<hov>%s, %a, %a, %S@]);" assignee pp_indexes idcs
+          pp_expr rhs
+          (strf "assigning variable %a" pp_indexed_simple (assignee, idcs))
+    | TargetPE e -> pf ppf "lp_accum__.add(%a);" pp_expr e
+    | NRFunApp
+        (CompilerInternal, fname, {expr= Lit (Str, check_name); _} :: args)
+      when fname = string_of_internal_fn FnCheck ->
+        let args = {expr= Var "function__"; emeta= internal_meta} :: args in
+        pp_statement ppf
+          { stmt= NRFunApp (CompilerInternal, "check_" ^ check_name, args)
+          ; smeta }
+    | NRFunApp (CompilerInternal, fname, [var])
+      when fname = string_of_internal_fn FnWriteParam ->
+        pf ppf "vars__.push_back(@[<hov>%a@]);" pp_expr var
+    | NRFunApp (CompilerInternal, fname, args) ->
+        let fname, extra_args = trans_math_fn fname in
+        pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr)
+          (extra_args @ args)
+    | NRFunApp (StanLib, fname, args) ->
+        pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr) args
+    | NRFunApp (UserDefined, fname, args) ->
+        pf ppf "%s(@[<hov>%a@]);" fname (list ~sep:comma pp_expr) args
+    | Break -> string ppf "break;"
+    | Continue -> string ppf "continue;"
+    | Return e -> pf ppf "return %a;" (option pp_expr) e
+    | Skip -> ()
+    | IfElse (cond, ifbranch, elsebranch) ->
+        let pp_else ppf x = pf ppf "else %a" pp_statement x in
+        pf ppf "if (@[<hov>%a@]) %a %a" pp_expr cond pp_block_s ifbranch
+          (option pp_else) elsebranch
+    | While (cond, body) ->
+        pf ppf "while (@[<hov>%a@]) %a" pp_expr cond pp_block_s body
+    | For
+        { body=
+            { stmt= Assignment (_, {expr= FunApp (CompilerInternal, f, _); _}); _
+            } as body; _ }
+      when internal_fn_of_string f = Some FnReadParam ->
+        pp_statement ppf body
+        (* Skip For loop part, just emit body due to the way FnReadParam emits *)
+    | For {loopvar; lower; upper; body} ->
+        pp_for_loop ppf (loopvar, lower, upper, pp_statement, body)
+    | Block ls -> pp_block ppf (pp_stmt_list, ls)
+    | SList ls -> pp_stmt_list ppf ls
+    | Decl {decl_adtype; decl_id; decl_type} ->
+        pp_possibly_sized_decl ppf (decl_id, decl_type, decl_adtype)
+  in
+  pf ppf "{@;<1 2>@[<v>%a%a@]@,}" Locations.pp_smeta smeta pp_statement_body
+    stmt
 
 and pp_block_s ppf body =
   match body.stmt with
