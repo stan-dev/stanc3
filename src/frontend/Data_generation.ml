46d686e3592a212c393b218a6257e9b530df9289
Matthijs Vakar <matthijsvakar@gmail.com>
2019-06-25 07:41:45 -0400

    Fixing real bounds

--- src/frontend/Data_generation.ml
+++ src/frontend/Data_generation.ml
@@ -3,15 +3,18 @@ open Middle
 open Ast
 open Fmt
 
-let rec unwrap_int_exn m e =
+let rec unwrap_num_exn m e =
 match e.expr with
-| IntNumeral s -> Int.of_string s
+| IntNumeral s -> Float.of_string s
+| RealNumeral s -> Float.of_string s
 | Variable s when Map.mem m s.name ->
-    unwrap_int_exn m (Map.find_exn m s.name)
+    unwrap_num_exn m (Map.find_exn m s.name)
 (* TODO: insert partial evaluation here *)
 | _ -> raise_s [%sexp ("Cannot convert size to number." : string)]
 
-let gen_num m (t : untyped_expression transformation) =
+let unwrap_int_exn m e = Int.of_float (unwrap_num_exn m e)
+
+let gen_num_int m (t : untyped_expression transformation) =
 let def_low, diff = 2, 5 in
 let low, up = match t with
 | Lower e -> unwrap_int_exn m e, unwrap_int_exn m e + diff
@@ -20,7 +23,16 @@ let low, up = match t with
 | _ -> def_low, def_low + diff
 in 
 Random.int (up - low + 1) + low
-(* TODO: do similar kind of generation for real bounds *)
+
+let gen_num_real m (t : untyped_expression transformation) =
+let def_low, diff = 2., 5. in
+let low, up = match t with
+| Lower e -> unwrap_num_exn m e, unwrap_num_exn m e +. diff
+| Upper e -> unwrap_num_exn m e -. diff, unwrap_num_exn m e
+| LowerUpper (e1, e2) -> unwrap_num_exn m e1, unwrap_num_exn m e2
+| _ -> def_low, def_low +. diff
+in 
+Random.float_range low up
 
 let rec repeat n e = match n with 0 -> [] | m -> e :: repeat (m - 1) e
 
@@ -29,10 +41,10 @@ let rec repeat_th n f =
 
 let int_n n = {expr= IntNumeral (Int.to_string n); emeta= {loc= no_span}}
 let int_two = int_n 2
-let gen_int m t = int_n (gen_num m t)
+let gen_int m t = int_n (gen_num_int m t)
 
 let gen_real m t =
-  {int_two with expr= RealNumeral (string_of_int (gen_num m t) ^ ".")}
+  {int_two with expr= RealNumeral (string_of_float (gen_num_real m t) ^ ".")}
 
 let gen_row_vector m n t =
   {int_two with expr= RowVectorExpr (repeat_th n (fun _ -> (gen_real m t)))}
@@ -155,7 +167,7 @@ let%expect_test "whole program data generation check" =
       \nD <- 1\
       \nN <- 4\
       \ny <- structure(c(0, 1, 1, 0), .Dim=c(4, 1))\
-      \nx <- structure(c(7., 3., 2., 6., 3., 2., 4., 5., 7., 5., 5., 6., 5., 6., 5., 5., 5., 7., 5., 5., 3., 2., 3., 7., 3., 4., 6., 3.), .Dim=c(4, 7))" |}]
+      \nx <- structure(c(4.80196289276., 2.50029771701., 6.99021511591., 4.22162060098., 4.70796052699., 4.39608595341., 6.36428059042., 4.56121913812., 2.75342745198., 2.1638430051., 4.10860520963., 6.40024614103., 4.52757732783., 3.32785186014., 4.87959893397., 6.50768784015., 4.26274555715., 6.75512163263., 4.83062840064., 4.25261591223., 4.19269169649., 4.62839240396., 5.18401718752., 3.82634901315., 6.32804481242., 5.68173766562., 2.36527695481., 4.53572899106.), .Dim=c(4, 7))" |}]
 
 let%expect_test "data generation check" =
   let expr =
@@ -189,14 +201,14 @@ let%expect_test "data generation check" =
   print_s [%sexp (str : string)] ;
   [%expect
     {|
-      "structure(c(2., 2., 6., 2., 5., 5., 2., 7., 3., 2., 6., 3.), .Dim=c(3, 4))" |}]
+      "structure(c(4.18152781994., 6.801766436., 4.84417841268., 4.25312636945., 5.20154190324., 2.71039449004., 3.32826213258., 2.56799363086., 4.07599383565., 3.60440575089., 6.0288479434., 3.54368914437.), .Dim=c(3, 4))" |}]
 
 let%expect_test "data generation check" =
   let expr = generate_value Map.Poly.empty (SVector (int_n 3)) Identity in
   let str = print_value_r expr in
   print_s [%sexp (str : string)] ;
   [%expect {|
-      "c(2., 2., 6.)" |}]
+      "c(4.18152781994., 6.801766436., 4.84417841268.)" |}]
 
 let%expect_test "data generation check" =
   let expr =
@@ -208,4 +220,4 @@ let%expect_test "data generation check" =
   print_s [%sexp (str : string)] ;
   [%expect
     {|
-      "structure(c(2., 2., 6., 2., 5., 5., 2., 7., 3., 2., 6., 3.), .Dim=c(4, 3))" |}]
+      "structure(c(4.18152781994., 6.801766436., 4.84417841268., 4.25312636945., 5.20154190324., 2.71039449004., 3.32826213258., 2.56799363086., 4.07599383565., 3.60440575089., 6.0288479434., 3.54368914437.), .Dim=c(4, 3))" |}]
