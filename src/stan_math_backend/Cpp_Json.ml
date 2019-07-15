581bc34acd43126b886d9432c449eb0320b3cf57
Sean Talts <sean.talts@gmail.com>
2019-07-15 09:38:43 -0400

    Forgot to add new files for JSON CPP

---
+++ src/stan_math_backend/Cpp_Json.ml
@@ -0,0 +1,47 @@
+open Core_kernel
+open Middle
+
+let rec sizedtype_to_json (st : mtype_loc_ad with_expr sizedtype) :
+    Yojson.Basic.t =
+  let emit_cpp_expr e = Fmt.strf "<< %a >>" Expression_gen.pp_expr e in
+  match st with
+  | SInt -> `Assoc [("name", `String "int")]
+  | SReal -> `Assoc [("name", `String "real")]
+  | SVector d | SRowVector d ->
+      `Assoc [("name", `String "vector"); ("length", `String (emit_cpp_expr d))]
+  | SMatrix (d1, d2) ->
+      `Assoc
+        [ ("name", `String "matrix")
+        ; ("rows", `String (emit_cpp_expr d1))
+        ; ("cols", `String (emit_cpp_expr d2)) ]
+  | SArray (st, d) ->
+      `Assoc
+        [ ("name", `String "array")
+        ; ("length", `String (emit_cpp_expr d))
+        ; ("element_type", sizedtype_to_json st) ]
+
+let replace_cpp_expr s =
+  s
+  |> Str.global_replace (Str.regexp "\"") "\\\""
+  |> Str.global_replace (Str.regexp "\\\\\"<<") "\" <<"
+  |> Str.global_replace (Str.regexp ">>\\\\\"") "<< \""
+
+let out_var_json (name, st, block) : Yojson.Basic.t =
+  `Assoc
+    [ ("name", `String name)
+    ; ("type", sizedtype_to_json st)
+    ; ("block", `String (Fmt.strf "%a" Pretty.pp_io_block block)) ]
+
+let wrap_in_quotes s = "\"" ^ s ^ "\""
+
+let out_var_interpolated_json_str vars =
+  `List (List.map ~f:out_var_json vars)
+  |> Yojson.Basic.to_string |> replace_cpp_expr |> wrap_in_quotes
+
+let%expect_test "outvar to json" =
+  let var x = {expr= Var x; emeta= internal_meta} in
+  [("var_one", SArray (SVector (var "N"), var "K"), Parameters)]
+  |> out_var_interpolated_json_str |> print_endline ;
+  [%expect
+    {|
+    "[{\"name\":\"var_one\",\"type\":{\"name\":\"array\",\"length\":" << K << ",\"element_type\":{\"name\":\"vector\",\"length\":" << N << "}},\"block\":\"parameters\"}]" |}]
