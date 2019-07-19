f25420bfa4bd18f68f12d229cc30e538fef9455d
Sean Talts <sean.talts@gmail.com>
2019-07-19 06:58:18 -0400

    TEMP init

---
+++ src/tfp_backend/Code_gen.ml
@@ -0,0 +1,35 @@
+open Core_kernel
+open Middle
+open Fmt
+
+let rec pp_expr ppf {expr; _} = match expr with
+  | Var ident -> string ppf ident
+  | Lit (Str, s) -> pf ppf "%S" s
+  | Lit (_, s) -> string ppf s
+  | FunApp (_, fname, args) ->
+    pf ppf "%s(@[<hov>%a@])" fname (list ~sep:comma pp_expr) args
+  | TernaryIf (cond, iftrue, iffalse) ->
+    pf ppf "%a if %a else %a" pp_expr cond pp_expr iftrue pp_expr iffalse
+  | EAnd (a, b) -> pf ppf "%a and %a" pp_expr a pp_expr b
+  | EOr (a, b) -> pf ppf "%a and %a" pp_expr a pp_expr b
+  | Indexed (lhs, indices) -> (??)
+
+let dist_prefix = "tfd.distributions."
+let pass_through_fnames = String.Set.of_list ["normal"]
+
+let rec translate_funapps {expr; emeta} =
+  let expr = match expr with
+  | FunApp(StanLib, fname, args) ->
+    let prefix = if Utils.is_distribution_name fname then dist_prefix else "" in
+    let fname', args' = (match fname with
+      | "multi_normal_cholesky" -> "MultivariateNormalTriL", args
+      | _ ->
+        if Set.mem pass_through_fnames fname then fname, args else
+        raise_s [%message "Not sure how to handle " fname " yet!"]
+    ) in
+    FunApp(StanLib, prefix ^ fname', args')
+  | _ -> map_expr translate_funapps expr
+  in
+  {expr; emeta}
+
+let trans_prog = map_prog translate_funapps Fn.id
