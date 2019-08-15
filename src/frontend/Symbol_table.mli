424e28bed3974cedd26086c245a38c8612f76cc4
Palmer Lao <palmer.lao@gmail.com>
2019-08-15 16:23:37 -0700

    Make interface pure

--- src/frontend/Symbol_table.mli
+++ src/frontend/Symbol_table.mli
@@ -5,29 +5,29 @@ type 'a state
 val initialize : unit -> 'a state
 (** Creates a new symbol table *)
 
-val enter : 'a state -> string -> 'a -> unit
+val enter : 'a state -> string -> 'a -> 'a state
 (** Enters a specified identifier with its specified type (or other) information
     into a symbol table  *)
 
 val look : 'a state -> string -> 'a option
 (** Looks for an identifier in a symbol table and returns its information if found and None otherwise  *)
 
-val begin_scope : 'a state -> unit
+val begin_scope : 'a state -> 'a state
 (** Used to start a new local scope which symbols added from now will end up in *)
 
-val end_scope : 'a state -> unit
+val end_scope : 'a state -> 'a state
 (** Used to end a local scope, purging the symbol table of all symbols added in that scope *)
 
-val set_read_only : 'a state -> string -> unit
+val set_read_only : 'a state -> string -> 'a state
 (** Used to add a read only label to an identifier *)
 
 val get_read_only : 'a state -> string -> bool
 (** Used to check for a read only label for an identifier *)
 
-val set_is_assigned : 'a state -> string -> unit
+val set_is_assigned : 'a state -> string -> 'a state
 (** Label an identifier as having been assigned to *)
 
-val set_is_unassigned : 'a state -> string -> unit
+val set_is_unassigned : 'a state -> string -> 'a state
 (** Label an identifier as not having been assigned to *)
 
 val check_is_unassigned : 'a state -> string -> bool
@@ -39,5 +39,5 @@ val check_some_id_is_unassigned : 'a state -> bool
 val is_global : 'a state -> string -> bool
 (** Used to check whether an identifier was declared in global scope *)
 
-val unsafe_clear_symbol_table : 'a state -> unit
+val unsafe_clear_symbol_table : 'a state -> 'a state
 (** Used to clear the whole symbol table *)
