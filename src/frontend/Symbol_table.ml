62c311984c97d558e49bd563ea2db583a2f4a9af
Palmer Lao <palmer.lao@gmail.com>
2019-08-17 18:33:40 -0700

    Use Set instead of Map string unit

--- src/frontend/Symbol_table.ml
+++ src/frontend/Symbol_table.ml
@@ -8,17 +8,17 @@ type 'a state =
   { table: (string, 'a, String.comparator_witness) Map.t
   ; stack: string List.t
   ; scopedepth: int
-  ; readonly: (string, unit, String.comparator_witness) Map.t
-  ; isunassigned: (string, unit, String.comparator_witness) Map.t
-  ; globals: (string, unit, String.comparator_witness) Map.t }
+  ; readonly: (string, String.comparator_witness) Set.t
+  ; isunassigned: (string, String.comparator_witness) Set.t
+  ; globals: (string, String.comparator_witness) Set.t }
 
 let empty =
   { table= Map.empty (module String)
   ; stack= []
   ; scopedepth= 0
-  ; readonly= Map.empty (module String)
-  ; isunassigned= Map.empty (module String)
-  ; globals= Map.empty (module String) }
+  ; readonly= Set.empty (module String)
+  ; isunassigned= Set.empty (module String)
+  ; globals= Set.empty (module String) }
 
 let sentinel_new_scope = "-sentinel-new-scope-"
 
@@ -29,7 +29,7 @@ let add_ignoring_dup map k v = match Map.add map ~key:k ~data:v with
 let enter s str ty =
   let new_globals =
     if s.scopedepth = 0
-    then add_ignoring_dup s.globals str ()
+    then Set.add s.globals str
     else s.globals
   in
   let new_table = add_ignoring_dup s.table str ty in
@@ -52,10 +52,11 @@ let end_scope s =
   let new_scopedepth = s.scopedepth - 1 in
   let (old_vars_list, sentinel_and_keep_vars) = List.split_while s.stack ~f:(fun var -> var <> sentinel_new_scope) in
   let old_vars_set = String.Set.of_list old_vars_list in
-  let pop_old_vars map = Map.filter_keys map ~f:(fun key -> Set.mem old_vars_set key) in
-  let new_table = pop_old_vars s.table in
-  let new_readonly = pop_old_vars s.readonly in
-  let new_isunassigned = pop_old_vars s.isunassigned in
+  let pop_old_vars_map map = Map.filter_keys map ~f:(fun key -> Set.mem old_vars_set key) in
+  let pop_old_vars_set set = Set.diff set old_vars_set in
+  let new_table = pop_old_vars_map s.table in
+  let new_readonly = pop_old_vars_set s.readonly in
+  let new_isunassigned = pop_old_vars_set s.isunassigned in
   let new_stack = List.tl_exn sentinel_and_keep_vars in
   { s with table= new_table
          ; readonly= new_readonly
@@ -64,19 +65,19 @@ let end_scope s =
          ; stack= new_stack }
 
 let set_read_only s str =
-  { s with readonly= add_ignoring_dup s.readonly str () }
+  { s with readonly= Set.add s.readonly str }
 
 let get_read_only s str =
-  match Map.find s.readonly str with Some () -> true | _ -> false
+  Set.mem s.readonly str
 
 let set_is_assigned s str =
-  { s with isunassigned= Map.remove s.isunassigned str }
+  { s with isunassigned= Set.remove s.isunassigned str }
 
 let set_is_unassigned s str =
-  { s with isunassigned= add_ignoring_dup s.isunassigned str () }
+  { s with isunassigned= Set.add s.isunassigned str }
 
-let check_is_unassigned s str = Map.mem s.isunassigned str
-let check_some_id_is_unassigned s = not (Map.length s.isunassigned = 0)
+let check_is_unassigned s str = Set.mem s.isunassigned str
+let check_some_id_is_unassigned s = not (Set.length s.isunassigned = 0)
 
 let is_global s str =
-  match Map.find s.globals str with Some _ -> true | _ -> false
+  Set.mem s.globals str
