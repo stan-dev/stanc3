de996726f42590f5b0778203621ff8adbb756240
Michael Thomas <mthomas180@gmail.com>
2019-09-18 15:19:57 +0100

    Common library

---
+++ src/common/Common.ml
@@ -0,0 +1,15 @@
+(* Type-class like signatures *)
+module Foldable = Foldable
+
+(* Other signatures *)
+module Pretty = Pretty
+
+(* 'Two-level type' signatures and functors *)
+module Pattern = Pattern
+module Fixed = Fixed
+module Label = Label
+module Specialized = Specialized
+
+(* General helpers *)
+module Gensym = Gensym
+module Helpers = Helpers
