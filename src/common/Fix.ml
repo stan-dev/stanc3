0f76d0d5fdbea80eab93c0a9e5dde8a8c115a1da
Michael Thomas <mthomas180@gmail.com>
2019-09-18 11:00:02 +0100

    remove fold_map

--- src/common/Fix.ml
+++ src/common/Fix.ml
@@ -24,13 +24,15 @@ module type S = sig
 
   `type 'a pattern = Leaf of string | Branch of 'a * 'a`
 
-  we can use `fold_pattern` to calculate the maximum depth of fixed type:
+  we can use `fold_pattern` to calculate the maximum depth of data structure 
+  obtained by `fixing` the pattern type:
 
   ```
   let max_depth t = 
     let f = function 
         | meta , Leaf _ -> 1 
-        | meta , Branch(left_depth,right_depth) -> 1 + (max left_depth right_depth)
+        | meta , Branch(left_depth,right_depth) -> 
+            1 + (max left_depth right_depth)
     in
     fold_pattern ~f t
   ```
@@ -45,18 +47,9 @@ module type S = sig
   (** `rewrite_bottom_up` specializes `fold_pattern` so that the result type 
   `'r` is equal to the type of our fixed-point data structure i.e. `'r = 'a t`. 
   This also means that the function `f` can be written with our fixed-point type 
-  `'a t` as its arguement. *)
+  `'a t` as its argument. *)
 
-  val fold_pattern_ctxt : f:('a t -> 'a * 'r Pattern.t -> 'r) -> 'a t -> 'r
-  (** `fold_pattern_ctxt` generalized `fold_pattern` so that the function `f`
-  receives the 'parent' (or context) of the element of the data structure it
-  is currently operating on. *)
-
-  val rewrite_ctxt : f:('a t -> 'a t -> 'a t) -> 'a t -> 'a t
-  (** `rewrite_ctxt` specializes `fold_pattern_ctxt` so that the result type 
-  `'r` is equal to the type of our fixed-point data structure. *)
-
-  val unfold : f:('r -> 'a * 'r Pattern.t) -> 'r -> 'a t
+  val unfold_pattern : f:('r -> 'a * 'r Pattern.t) -> 'r -> 'a t
   (** `unfold` builds a fixed-point data structure from the top down. Starting
   with a user-supplied seed of type `'r`, `unfold` recursively applies the 
   function `f` yielding a tuple of meta-data and pattern with elements of 
@@ -72,29 +65,6 @@ module type S = sig
   (** `rewrite_top_down` specializes `unfold` by requiring that `'r = 'a t`. 
   As a consequence the function `f` accepts our record type `'a t` as its 
   argument. *)
-
-  val unfold_partial :
-    f:('r -> 'a * ('a t, 'r) Either.t Pattern.t) -> 'r -> 'a t
-  (** `unfold_partial` generalizes `unfold` by allowing the `unfold` to 
-  terminate at any point, rather than only when a pattern element without
-  a type `'r` is reached.
-
-  This early termination is encoded with `Either.t`; comparing the functions `f`
-  appearing in `unfold` and `unfold_partial`:
-
-  ```
-  'r -> 'a *        'r           Pattern.t 
-  'r -> 'a * ('a t, 'r) Either.t Pattern.t  
-  ```
-  
-  In the second case (`unfold_partial`) the function returns a tuple of 
-  meta-data and _either_ a fixed-point value `'a t` to which no further 
-  unfolding is applied _or_ a seed value `'r` to which `unfold_partial` is 
-  recursively applied. *)
-
-  val rewrite_partial : f:('a t -> ('a t, 'a t) Either.t) -> 'a t -> 'a t
-  (** `rewrite_partial` specializes `unfold_partial` by requiring that 
-  `'r = 'a t`. *)
 end
 
 module Make (Pattern : Pattern.S) : S with module Pattern := Pattern = struct
@@ -114,78 +84,142 @@ module Make (Pattern : Pattern.S) : S with module Pattern := Pattern = struct
                                 let fold = fold
   end)
 
-  let rec fold_pattern ~f x =
-    let meta, pattern = unfix x in
+  let rec fold_pattern ~f t =
+    let meta, pattern = unfix t in
     let pattern' = Pattern.map (fold_pattern ~f) pattern in
     f (meta, pattern')
 
-  let rewrite_bottom_up ~f x = fold_pattern ~f:(Fn.compose f fix) x
+  (** For clarity this is written explicitly but is equivalent to 
+  `fold_pattern ~f:(Fn.compose f fix) t` 
+  *)
+  let rec rewrite_bottom_up ~f t =
+    let x = {t with pattern= Pattern.map (rewrite_bottom_up ~f) t.pattern} in
+    f x
 
-  let rec fold_pattern_ctxt ~f t =
-    let meta, pattern = unfix t in
-    let pattern' = Pattern.map (fold_pattern_ctxt ~f) pattern in
-    f t (meta, pattern')
-
-  let rewrite_ctxt ~f t =
-    fold_pattern_ctxt ~f:(fun ctxt proj -> f ctxt @@ fix proj) t
-
-  let rec unfold ~f x =
+  let rec unfold_pattern ~f x =
     let meta, pattern = f x in
-    let pattern' = Pattern.map (unfold ~f) pattern in
+    let pattern' = Pattern.map (unfold_pattern ~f) pattern in
     fix (meta, pattern')
 
-  let rewrite_top_down ~f x = unfold ~f:(Fn.compose unfix f) x
-
-  let rec unfold_partial ~f x =
-    let meta, pattern = f x in
-    let g x = Either.value_map ~first:Fn.id ~second:(unfold_partial ~f) x in
-    let pattern' = Pattern.map g pattern in
-    fix (meta, pattern')
-
-  let rewrite_partial ~f x =
-    let align t =
-      let meta, pattern = unfix t in
-      let pattern' = Pattern.map Either.first pattern in
-      (meta, pattern')
-    in
-    let sequence t = Either.value_map ~first:align ~second:align t in
-    unfold_partial ~f:(Fn.compose sequence f) x
+  (** For clarity this is written explicitly but it is equivalent to
+  `unfold ~f:(Fn.compose unfix f) x` 
+  *)
+  let rec rewrite_top_down ~f x =
+    let t = f x in
+    {t with pattern= Pattern.map (rewrite_top_down ~f) t.pattern}
 end
 
 module type S2 = sig
   module First : S
   module Pattern : Pattern.S2
 
-  type ('a, 'b) t [@@deriving compare, map, fold, hash, sexp]
+  type ('a, 'b) t = {meta: 'b; pattern: ('a First.t, ('a, 'b) t) Pattern.t}
+  [@@deriving compare, map, fold, hash, sexp]
+
+  val fix : 'b * ('a First.t, ('a, 'b) t) Pattern.t -> ('a, 'b) t
+  val unfix : ('a, 'b) t -> 'b * ('a First.t, ('a, 'b) t) Pattern.t
+  val pattern : ('a, 'b) t -> ('a First.t, ('a, 'b) t) Pattern.t
+  val meta : ('a, 'b) t -> 'b
 
   include Foldable.S2 with type ('a, 'b) t := ('a, 'b) t
   include Pretty.S2 with type ('a, 'b) t := ('a, 'b) t
-
-  type ('a, 'b, 'r) algebra = 'b * ('a, 'r) Pattern.t -> 'r
 
   val fold_pattern :
        f:('a * 'r1 First.Pattern.t -> 'r1)
     -> g:('b * ('r1, 'r2) Pattern.t -> 'r2)
     -> ('a, 'b) t
     -> 'r2
+  (** 
+  `fold_pattern` traverses the data structure from the bottom up
+  replacing the original meta data of `First` with type `'a` with some result 
+  type `'r1` and the meta data at this level withtype `'b` to another result
+  type `'r2` and combines the result values on the way back up. 
+  *)
 
-  val transform_bottom_up :
-       ('a First.t -> 'a First.t)
-    -> (('a, 'b) t -> ('a, 'b) t)
+  val rewrite_bottom_up :
+       f:('a First.t -> 'a First.t)
+    -> g:(('a, 'b) t -> ('a, 'b) t)
     -> ('a, 'b) t
     -> ('a, 'b) t
-  (* type ('a, 'b, 'r1, 'r2) r_algebra =
-    ('a, 'b) t -> 'b * ('r1, 'r2) Pattern.t -> 'r2
+  (** `rewrite_bottom_up` specializes `fold_pattern` so that the result type 
+  `'r1` is equal to the type of the nested fixed-point type 
+  i.e. `'r1 = 'a First.t` and the result type `'r2` is equal to the top-level
+  fixed-point type i.e. `'r2 = ('a,'b) t`.
 
-  val para :
-       ('a, 'r1) First.r_algebra
-    -> ('a, 'b, 'r1, 'r2) r_algebra
-    -> ('a, 'b) t
+  This also means that the function `f` can be written with our nested 
+  fixed-point type  `'a First.t` as its argument and `g` can be written with 
+  `('a,'b) t` as its argument. 
+  *)
+
+  val unfold_pattern :
+       f:('r1 -> 'a * 'r1 First.Pattern.t)
+    -> g:('r2 -> 'b * ('r1, 'r2) Pattern.t)
     -> 'r2
-
-  val transform_with_context :
-       ('a First.t -> 'a First.t -> 'a First.t)
-    -> (('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t)
     -> ('a, 'b) t
-    -> ('a, 'b) t *)
+  (** `unfold_pattern` takes a seed value of type `'r2` and uses the function
+  `g` to generate a tuple of meta-data and a pattern with types `'r1` and `'r2`.
+  The functions proceeds by recursively applying `g` to the contained values of
+  type `'r2` and `f` to values of type `'r1` finishing when the pattern contains
+  no values of type `'r1` or `'r2`.
+  *)
+
+  val rewrite_top_down :
+       f:('a First.t -> 'a First.t)
+    -> g:(('a, 'b) t -> ('a, 'b) t)
+    -> ('a, 'b) t
+    -> ('a, 'b) t
+  (** `rewrite_top_down` specializes `unfold_pattern` in a manner analogous to
+  how `rewrite_bottom_up` specializes `fold_pattern` *)
+end
+
+module Make2 (First : S) (Pattern : Pattern.S2) :
+  S2 with module First := First and module Pattern := Pattern = struct
+  type ('a, 'b) t = {meta: 'b; pattern: ('a First.t, ('a, 'b) t) Pattern.t}
+  [@@deriving map, fold, compare, hash, sexp]
+
+  let unfix {meta; pattern} = (meta, pattern)
+  let fix (meta, pattern) = {meta; pattern}
+  let pattern {pattern; _} = pattern
+  let meta {meta; _} = meta
+
+  let rec pp f g ppf {pattern; meta} =
+    Fmt.pf ppf {|%a%a|} g meta (Pattern.pp (First.pp f) (pp f g)) pattern
+
+  include Foldable.Make2 (struct
+    type nonrec ('a, 'b) t = ('a, 'b) t
+
+    let fold = fold
+  end)
+
+  let rec fold_pattern ~f ~g t =
+    let meta, pattern = unfix t in
+    let pattern' =
+      Pattern.map (First.fold_pattern ~f) (fold_pattern ~f ~g) pattern
+    in
+    g (meta, pattern')
+
+  (** fold_pattern (Fn.compose f First.fix) (Fn.compose g fix) x *)
+  let rec rewrite_bottom_up ~f ~g t =
+    g
+      { t with
+        pattern=
+          Pattern.map
+            (First.rewrite_bottom_up ~f)
+            (rewrite_bottom_up ~f ~g) t.pattern }
+
+  let rec unfold_pattern ~f ~g x =
+    let meta, pattern = g x in
+    let pattern' =
+      Pattern.map (First.unfold_pattern ~f) (unfold_pattern ~f ~g) pattern
+    in
+    {meta; pattern= pattern'}
+
+  (**`unfold_pattern (Fn.compose First.unfix f) (Fn.compose unfix g) x`*)
+  let rec rewrite_top_down ~f ~g x =
+    let t = g x in
+    { t with
+      pattern=
+        Pattern.map
+          (First.rewrite_top_down ~f)
+          (rewrite_top_down ~f ~g) t.pattern }
 end
--- src/common/Foldable.ml
+++ src/common/Foldable.ml
@@ -15,8 +15,7 @@ signatures and functors e.g. `Basic2`, `S2` and `Make2`
 *)
 open Core_kernel
 
-
-(** The `Basic` definition for type constructors with a single type variable. 
+(* The `Basic` definition for type constructors with a single type variable. 
 
 This signature just says that you have some type `'a t` and a function `fold` 
 with type: `fold: ('b -> 'a -> 'b) -> 'b -> 'a t -> 'b`. 
@@ -27,45 +26,30 @@ module type Basic = sig
   type 'a t [@@deriving fold]
 end
 
-(** The complete module signature for `Foldable` for types with a single 
+(* The complete module signature for `Foldable` for types with a single 
 type variable.
 *)
 module type S = sig
   (** A data structure which can be folded *)
   type 'a t
 
-  (** Map each element of the structure to a type over which a `Monoid` 
-  is defined and the combine using the associated `combine`function.
-
-  A `Monoid` is just another standard interface with says for some type `t` 
-  there is a function `combine` and some _value_ `empty` for which:
-
-  `combine x empty === x === combine empty x` 
-
-  for all values of `x`. 
-  *)
-  val fold_map :
-       (module Monoid.S with type t = 'a)
-    -> f:('b -> 'a)
-    -> ?init:'a
-    -> 'b t
-    -> 'a
-
+  val fold_left : f:('b -> 'a -> 'b) -> init:'b -> 'a t -> 'b
   (** Left associative fold of a data structure; this is the same as the 
   function derived from `[@@deriving fold]` but with labelled arguments *)
-  val fold_left : f:('b -> 'a -> 'b) -> init:'b -> 'a t -> 'b
-  (** Right associative fold of a data structure *)
+
   val fold_right : f:('a -> 'b -> 'b) -> init:'b -> 'a t -> 'b
+  (** Right associative fold of a data structure *)
+
+  val any : pred:('a -> bool) -> ?init:bool -> 'a t -> bool
   (** Test whether any element of the data structure satisfies the supplied 
   predicate. The optional argument `init` specifies the starting value and 
   defaults to `false`. *)
-  val any : pred:('a -> bool) -> ?init:bool -> 'a t -> bool
+
+  val all : pred:('a -> bool) -> ?init:bool -> 'a t -> bool
   (** Test whether all elements of the the data structure satify the supplied
   predicate. The optional argument `init` specifies the starting value and 
   defaults to `true`. *)
-  val all : pred:('a -> bool) -> ?init:bool -> 'a t -> bool
 end
-
 
 module Make (X : Basic) : S with type 'a t := 'a X.t = struct
   let fold_left ~f ~init x = X.fold f init x
@@ -75,39 +59,23 @@ module Make (X : Basic) : S with type 'a t := 'a X.t = struct
     let f' k x z = k @@ f x z in
     fold_left ~f:f' ~init:(fun x -> x) x init
 
-  (** `fold_map` in terms of right associative fold *)
-  let fold_map (type a) (module M : Monoid.S with type t = a) ~f
-      ?init:(empty = M.empty) x =
-    fold_right ~f:(fun x accu -> M.combine accu @@ f x) ~init:empty x
+  let any ~pred ?(init = false) x =
+    fold_right ~f:(fun x accu -> accu || pred x) ~init x
 
-  (** `any` written in terms of `fold_map` with the monoid (bool,false,||) *)
-  let any ~pred ?init x = fold_map (module Monoid.Bool_or) ~f:pred ?init x
-
-  (** `all` written in terms of `fold_map` with the monoid (bool,true,&&) *)
-  let all ~pred ?init x = fold_map (module Monoid.Bool_and) ~f:pred ?init x
+  let all ~pred ?(init = true) x =
+    fold_right ~f:(fun x accu -> accu && pred x) ~init x
 end
 
-
-
-
-(** The `Basic` definition for type constructors with two type variables. *)
+(* The `Basic` definition for type constructors with two type variables. *)
 module type Basic2 = sig
   type ('a, 'b) t [@@deriving fold]
 end
 
-(** The complete module signature for `Foldable` for types with a single 
+(* The complete module signature for `Foldable` for types with a single 
 type variable.
 *)
 module type S2 = sig
   type ('a, 'b) t
-
-  val fold_map :
-       (module Monoid.S with type t = 'a)
-    -> f:('b -> 'a)
-    -> g:('c -> 'a)
-    -> ?init:'a
-    -> ('b, 'c) t
-    -> 'a
 
   val fold_left :
     f:('c -> 'a -> 'c) -> g:('c -> 'b -> 'c) -> init:'c -> ('a, 'b) t -> 'c
@@ -137,16 +105,15 @@ module Make2 (X : Basic2) : S2 with type ('a, 'b) t := ('a, 'b) X.t = struct
     let f' k x z = k @@ f x z and g' k x z = k @@ g x z in
     fold_left ~f:f' ~g:g' ~init:Fn.id x init
 
-  let fold_map (type a) (module M : Monoid.S with type t = a) ~f ~g
-      ?init:(empty = M.empty) x =
+  let any ~pred_first ~pred_second ?(init = false) x =
     fold_right
-      ~f:(fun x accu -> M.combine accu @@ f x)
-      ~g:(fun x accu -> M.combine accu @@ g x)
-      ~init:empty x
+      ~f:(fun x accu -> accu || pred_first x)
+      ~g:(fun x accu -> accu || pred_second x)
+      ~init x
 
-  let any ~pred_first ~pred_second ?init x =
-    fold_map (module Monoid.Bool_or) ~f:pred_first ~g:pred_second ?init x
-
-  let all ~pred_first ~pred_second ?init x =
-    fold_map (module Monoid.Bool_and) ~f:pred_first ~g:pred_second ?init x
+  let all ~pred_first ~pred_second ?(init = true) x =
+    fold_right
+      ~f:(fun x accu -> accu && pred_first x)
+      ~g:(fun x accu -> accu && pred_second x)
+      ~init x
 end
