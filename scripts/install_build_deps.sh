3d4ab091696b4eefa50e903b78fb58cd98e01b05
Sean Talts <sean.talts@gmail.com>
2019-10-05 14:17:57 -0400

    Try not specifying dune for install

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -2,4 +2,4 @@
 # Menhir is our parsing library and annoyingly its module name does not match
 # its library name, so we install it manually here.
 opam pin -y ocamlformat 0.8
-opam install -y dune.1.10.0 core_kernel.v0.11.1 menhir.20181113 ppx_deriving.4.2.1 fmt.0.8.5 yojson.1.7.0
+opam install -y core_kernel.v0.11.1 menhir.20181113 ppx_deriving.4.2.1 fmt.0.8.5 yojson.1.7.0
