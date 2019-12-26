2626a6504f37ff17c94acb083a15ff334ef24a5e
Adam Haber <adamhaber@gmail.com>
2019-12-26 15:59:59 +0200

    added more distributions

--- src/tfp_backend/Transform_mir.ml
+++ src/tfp_backend/Transform_mir.ml
@@ -12,9 +12,12 @@ let remove_stan_dist_suffix s =
 
 let capitalize_fnames =
   String.Set.of_list
-    ["normal"; "cauchy"; "gumbel"; "exponential"; "gamma"; "beta"]
+    ["normal"; "cauchy"; "gumbel"; "exponential"; "gamma"; "beta"; "poisson"; "wishart"]
 
 let map_functions fname args =
+  let 
+    open Expr 
+  in
   match fname with
   | "multi_normal_cholesky" -> ("MultivariateNormalTriL", args)
   | "student_t" -> ("StudentT", args)
@@ -25,43 +28,44 @@ let map_functions fname args =
   | "lkj_corr_cholesky" -> ("CholeskyLKJ", args)
   | "binomial_logit" -> ("Binomial", args)
   | "bernoulli_logit" -> ("Bernoulli", args)
+  | "categorical_logit" -> ("Categorical", args)
+  | "von_mises" -> ("VonMises", args)
   | "binomial" -> (
-    match args with
-    | [y; n; p] ->
-        ( "Binomial"
-        , [ y; n
-          ; {Expr.Fixed.pattern= Var "None"; meta= Expr.Typed.Meta.empty}
-          ; p ] )
-    | _ ->
-        raise_s
-          [%message
-            " Binomial argument should contain exactly three elements."] )
+      match args with
+      | [y; n; p] -> ( "Binomial", [ y; n; {Fixed.pattern= Var "None"; meta= Typed.Meta.empty}; p ] )
+      | _ -> raise_s [%message " Binomial argument should contain exactly three elements."] )
   | "bernoulli" -> (
-    match args with
-    | [y; p] ->
-        ( "Bernoulli"
-        , [y; {Expr.Fixed.pattern= Var "None"; meta= Expr.Typed.Meta.empty}; p]
-        )
-    | _ ->
-        raise_s
-          [%message " Binomial argument should contain exactly two elements."]
-    )
+      match args with
+      | [y; p] -> ( "Bernoulli", [y; {Fixed.pattern= Var "None"; meta= Typed.Meta.empty}; p])
+      | _ -> raise_s [%message " Binomial argument should contain exactly two elements."] )
+  | "categorical" -> (
+      match args with
+      | [y; p] -> ( "Categorical", [y; {Fixed.pattern= Var "None"; meta= Typed.Meta.empty}; p])
+      | _ -> raise_s [%message " Categorical argument should contain exactly two elements."] )
+  | "poisson_log" -> (
+      match args with
+      | [y; log_lambda] -> ( "Poisson", [y; {Fixed.pattern= Var "None"; meta= Typed.Meta.empty}; log_lambda])
+      | _ -> raise_s [%message " Poisson argument should contain exactly two elements."] )
+  | "pareto" ->  (
+      match args with
+      | [y; y_min; alpha] -> ( "Categorical", [y; alpha; y_min])
+      | _ -> raise_s [%message " Pareto argument should contain exactly three elements."] )
   | f when Operator.of_string_opt f |> Option.is_some -> (fname, args)
   | _ ->
-      if Set.mem capitalize_fnames fname then (String.capitalize fname, args)
-      else raise_s [%message "Not sure how to handle " fname " yet!"]
+    if Set.mem capitalize_fnames fname then (String.capitalize fname, args)
+    else raise_s [%message "Not sure how to handle " fname " yet!"]
 
 let translate_funapps e =
   let open Expr.Fixed in
   let f ({pattern; _} as expr) =
     match pattern with
     | FunApp (StanLib, fname, args) ->
-        let prefix =
-          if Utils.is_distribution_name fname then dist_prefix else ""
-        in
-        let fname = remove_stan_dist_suffix fname in
-        let fname, args = map_functions fname args in
-        {expr with pattern= FunApp (StanLib, prefix ^ fname, args)}
+      let prefix =
+        if Utils.is_distribution_name fname then dist_prefix else ""
+      in
+      let fname = remove_stan_dist_suffix fname in
+      let fname, args = map_functions fname args in
+      {expr with pattern= FunApp (StanLib, prefix ^ fname, args)}
     | _ -> expr
   in
   rewrite_bottom_up ~f e
@@ -91,8 +95,8 @@ let rec remove_unused_stmts s =
     match s.Stmt.Fixed.pattern with
     | Assignment (_, {Expr.Fixed.pattern= FunApp (CompilerInternal, f, _); _})
       when Internal_fun.to_string FnConstrain = f
-           || Internal_fun.to_string FnUnconstrain = f ->
-        Stmt.Fixed.Pattern.Skip
+        || Internal_fun.to_string FnUnconstrain = f ->
+      Stmt.Fixed.Pattern.Skip
     | Decl _ -> Stmt.Fixed.Pattern.Skip
     | x -> Stmt.Fixed.Pattern.map Fn.id remove_unused_stmts x
   in
