b08d15dde1ec60fe03944041011e7f3cf4877a7e
Adam Haber <adamhaber@gmail.com>
2020-02-09 15:42:31 +0200

    test now includes warnings output

--- test/integration/tfp/tfp_models/python_kwrds.py
+++ test/integration/tfp/tfp_models/python_kwrds.py
@@ -1,6 +1,27 @@
 Info: Found int division at 'stan_models/python_kwrds.stan', line 13, column 12 to column 18:
   lambda / 3
 Positive values rounded down, negative values rounded up or down in platform-dependent way.
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier lambda is a reserved word in python, renamed to lambda__
+Identifier yield is a reserved word in python, renamed to yield__
+Identifier await is a reserved word in python, renamed to await__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier yield is a reserved word in python, renamed to yield__
+Identifier lambda is a reserved word in python, renamed to lambda__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier lambda is a reserved word in python, renamed to lambda__
+Identifier await is a reserved word in python, renamed to await__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier finally is a reserved word in python, renamed to finally__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier assert is a reserved word in python, renamed to assert__
+Identifier lambda is a reserved word in python, renamed to lambda__
 
 import numpy as np__
 import tensorflow as tf__
@@ -9,27 +30,27 @@ tfd__ = tfp__.distributions
 tfb__ = tfp__.bijectors
 from tensorflow.python.ops.parallel_for import pfor as pfor__
 
-def yield_(b):
+def yield__(b):
   return b * tf__.cast(3, tf__.float64)
    
-def func(await_):
-  return await_ + tf__.cast(1, tf__.float64)
+def func(await__):
+  return await__ + tf__.cast(1, tf__.float64)
    
 class python_kwrds_model(tfd__.Distribution):
 
-  def __init__(self, lambda_):
-    self.lambda_ = lambda_
+  def __init__(self, lambda__):
+    self.lambda__ = lambda__
      
   
   def log_prob_one_chain(self, params):
     target = 0
-    lambda_ = self.lambda_
-    finally_ = tf__.cast(params[0], tf__.float64)
-    assert_ = finally_ + tf__.cast(2, tf__.float64)
-    target += tf__.reduce_sum(tfd__.Normal(yield_(assert_),
+    lambda__ = self.lambda__
+    finally__ = tf__.cast(params[0], tf__.float64)
+    assert__ = finally__ + tf__.cast(2, tf__.float64)
+    target += tf__.reduce_sum(tfd__.Normal(yield__(assert__),
                                            tf__.cast(1, tf__.float64)).log_prob(d))
     target += tf__.reduce_sum(tfd__.Binomial(tf__.cast(10, tf__.float64),
-                                             func(finally_)).log_prob(lambda_))
+                                             func(finally__)).log_prob(lambda__))
     return target
      
   def log_prob(self, params):
@@ -37,14 +58,14 @@ class python_kwrds_model(tfd__.Distribution):
     
      
   def parameter_shapes(self, nchains__):
-    lambda_ = self.lambda_
+    lambda__ = self.lambda__
     return [(nchains__, )]
      
   def parameter_bijectors(self):
-    lambda_ = self.lambda_
+    lambda__ = self.lambda__
     return [tfb__.Identity()]
      
   def parameter_names(self):
-    return ["finally_"]
+    return ["finally__"]
      
 model = python_kwrds_model
