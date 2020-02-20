721d84ef01dd8bb8914b52257bcbcd3424d722aa
Charles Margossian <charlesm@metrumrg.com>
2020-02-20 09:47:54 -0500

    Add signature where x can be passed as a matrix.

--- src/middle/Stan_math_signatures.ml
+++ src/middle/Stan_math_signatures.ml
@@ -1495,7 +1495,18 @@ let () =
       ; (AutoDiffable, UVector); (DataOnly, UArray UVector)
       ; (DataOnly, UArray UReal); (DataOnly, UArray UInt)
       ; (AutoDiffable, UVector); (DataOnly, UReal); (DataOnly, UInt) ] ) ;
-    (* Embedded Laplace approximation *)
+  add_qualified
+    ( "laplace_marginal_bernoulli"
+    , ReturnType UReal
+    , [ (DataOnly, UArray UInt); (DataOnly, UArray UInt)
+      ; ( AutoDiffable
+        , UFun
+          ( [ (AutoDiffable, UVector); (DataOnly, UMatrix)
+            ; (DataOnly, UArray UReal); (DataOnly, UArray UInt) ]
+          , ReturnType UMatrix ) )
+          ; (AutoDiffable, UVector); (DataOnly, UMatrix)
+          ; (DataOnly, UArray UReal); (DataOnly, UArray UInt)
+          ; (AutoDiffable, UVector) ] ) ;
   add_qualified
     ( "laplace_marginal_poisson"
     , ReturnType UReal
--- test/integration/good/function-signatures/math/functions/laplace_marginal_bernoulli.stan
+++ test/integration/good/function-signatures/math/functions/laplace_marginal_bernoulli.stan
@@ -3,6 +3,11 @@ functions {
     matrix[1, 1] covariance;
     return covariance;
   }
+  
+  matrix Km (vector phi, matrix x, real[] delta, int[] delta_int) {
+    matrix[1, 1] covariance;
+    return covariance;
+  }
 }
 
 transformed data {
@@ -11,6 +16,7 @@ transformed data {
 
   vector[1] phi;
   vector[1] x[1];
+  matrix[1, 1] x_m;
   real delta[1];
   int delta_int[1];
   
@@ -28,5 +34,9 @@ model {
   //                              theta0, 1e-3, 100);
   target +=
     laplace_marginal_bernoulli(y, n_samples, K, phi, x, delta, delta_int,
+                               theta0);
+
+  target +=
+    laplace_marginal_bernoulli(y, n_samples, Km, phi, x_m, delta, delta_int,
                                theta0);
 }
