4c0e66cca53e0ad2407bb5cf02b08e06d5268a1f
Nicusor Serban <serban.nicusor@toptal.com>
2020-07-18 22:19:33 +0200

    Removed ocamlformat.0.8 from build deps

--- scripts/install_build_deps_windows.sh
+++ scripts/install_build_deps_windows.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Menhir is our parsing library and annoyingly its module name does not match
-# its library name, so we install it manually here.
-opam pin -y ocamlformat 0.8
-opam pin -y dune 1.11.4
+# Pin dune to 1.11.3 as newer and 2+ do not work for ocaml-windows64 !
+opam pin -y dune 1.11.3
 
 # Add windows repository
 opam repository add windows git://github.com/ocaml-cross/opam-cross-windows
@@ -13,11 +11,6 @@ opam install conf-flambda-windows
 
 # Install the compiler
 opam install "ocaml-windows64<=4.07.0"
-
-# Debug
-dune --version
-opam --version
-ocaml -version
 
 # Install dependencies
 opam install -y core_kernel-windows menhir-windows ppx_deriving-windows fmt-windows yojson-windows
