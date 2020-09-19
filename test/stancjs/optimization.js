b870a4afb4ef37c1d66702dac5dae3fa93be7ce2
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-09-19 21:43:52 +0200

    more tests

--- test/stancjs/optimization.js
+++ test/stancjs/optimization.js
@@ -40,12 +40,51 @@ data {
 `
 
 var no_opencl_test = stanc.stanc("no_opencl", glm_model, "");
+utils.print_error(no_opencl_test)
 var ind = no_opencl_test.result.search("matrix_cl<int> y_opencl__");
 if (ind > -1) {
     console.log("ERROR: OpenCL code found without --use-opencl flag!")
 }
 
 var opencl_test = stanc.stanc("opencl", glm_model, "--use-opencl");
+utils.print_error(opencl_test)
+var ind = opencl_test.result.search("matrix_cl<int> y_opencl__");
+if (ind == -1) {
+    console.log("ERROR: No OpenCL code found with --use-opencl flag!")
+}
+
+// multiple flags
+
+var glm_model2 = `
+functions {
+    int foo(real a);
+}
+
+data {
+    int<lower=1> k;
+    int<lower=0> n;
+    matrix[n, k] X;
+    int y[n];
+}
+
+transformed data {
+    real p = foo(5.0);
+}
+
+parameters {
+    vector[k] beta;
+    real alpha;
+} 
+
+model {
+    target += bernoulli_logit_glm_lpmf(y | X, alpha, beta);
+}
+`
+var opencl_test = stanc.stanc("opencl", glm_model2, "--use-opencl");
+utils.print_error(opencl_test)
+
+var opencl_test = stanc.stanc("opencl", glm_model2, "--use-opencl --allow-undefined");
+utils.print_error(opencl_test)
 var ind = opencl_test.result.search("matrix_cl<int> y_opencl__");
 if (ind == -1) {
     console.log("ERROR: No OpenCL code found with --use-opencl flag!")
--- test/stancjs/version.js
+++ test/stancjs/version.js
@@ -12,5 +12,10 @@ model {
     y ~ std_normal();
 }
 `
+
+
 let version_model = stanc.stanc("version-test", basic_model, "--version");
 utils.print_result(version_model)
+
+let version_model_multiple_flags = stanc.stanc("version-test", basic_model, "--warn-pedantic --version");
+utils.print_result(version_model_multiple_flags)
