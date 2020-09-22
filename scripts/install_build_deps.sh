01b93935914f6cdc39ff952dea68bc59e49518d9
Sean Talts <sean.talts@gmail.com>
2020-09-21 22:07:09 -0400

    Cleanup - ocamlformat is not used in static builds

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -4,6 +4,5 @@
 # its library name, so we install it manually here.
 
 opam pin -y dune 1.11.4
-opam pin -y ocamlformat 0.8
 
 opam install -y core_kernel.v0.11.1 menhir.20181113 ppx_deriving.4.2.1 fmt.0.8.5 yojson.1.7.0
--- scripts/install_dev_deps.sh
+++ scripts/install_dev_deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Merlin, utop, ocp-indent, ocamlformat, and patdiff are all for developer assistance
+opam pin -y ocamlformat 0.8
 opam install -y ocamlformat.0.8 merlin utop ocp-indent patdiff
