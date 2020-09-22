3f03c611b58b8dd2413a986f2d9b7e756d2d1697
Sean Talts <sean.talts@gmail.com>
2020-09-21 21:14:14 -0400

    Try to fix static build

--- scripts/install_dev_deps.sh
+++ scripts/install_dev_deps.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Merlin, utop, ocp-indent, ocamlformat, and patdiff are all for developer assistance
-opam install -y ocamlformat.0.8 merlin utop ocp-indent patdiff
+opam pin -y ocamlformat 0.8
+opam install -y merlin utop ocp-indent patdiff
