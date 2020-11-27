7d9c5c034b6f861177502bc5ef17e72b2cd4269e
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-11-27 15:02:57 +0100

    formatting and add stan:: to value_type_t

--- src/stan_math_backend/Stan_math_code_gen.ml
+++ src/stan_math_backend/Stan_math_code_gen.ml
@@ -70,7 +70,7 @@ let maybe_templated_arg_types (args : Program.fun_arg_decl) =
 let return_arg_types (args : Program.fun_arg_decl) =
   List.mapi args ~f:(fun i ((_, _, ut) as a) ->
       if UnsizedType.is_eigen_type ut && arg_needs_template a then
-        Some (sprintf "value_type_t<T%d__>" i)
+        Some (sprintf "stan::value_type_t<T%d__>" i)
       else if arg_needs_template a then Some (sprintf "T%d__" i)
       else None )
 
--- test/unit/Stan_math_code_gen_tests.ml
+++ test/unit/Stan_math_code_gen_tests.ml
@@ -27,8 +27,8 @@ let%expect_test "udf" =
     template <typename T0__, typename T1__>
     void
     sars(const T0__& x_arg__, const T1__& y_arg__, std::ostream* pstream__) {
-      using local_scalar_t__ = stan::promote_args_t<value_type_t<T0__>,
-              value_type_t<T1__>>;
+      using local_scalar_t__ = stan::promote_args_t<stan::value_type_t<T0__>,
+              stan::value_type_t<T1__>>;
       const auto& x = to_ref(x_arg__);const auto& y = to_ref(y_arg__);
       const static bool propto__ = true;
       (void) propto__;
@@ -62,7 +62,11 @@ let%expect_test "udf-expressions" =
   let pp_fun_def_w_rs a b = pp_fun_def a b String.Set.empty String.Set.empty in
   { fdrt= Some UMatrix
   ; fdname= "sars"
-  ; fdargs= [(DataOnly, "x", UMatrix); (AutoDiffable, "y", URowVector);(AutoDiffable, "z", URowVector); (AutoDiffable, "w", UArray UMatrix)]
+  ; fdargs=
+      [ (DataOnly, "x", UMatrix)
+      ; (AutoDiffable, "y", URowVector)
+      ; (AutoDiffable, "z", URowVector)
+      ; (AutoDiffable, "w", UArray UMatrix) ]
   ; fdbody=
       Stmt.Fixed.Pattern.Return
         (Some
@@ -75,15 +79,15 @@ let%expect_test "udf-expressions" =
   [%expect
     {|
     template <typename T0__, typename T1__, typename T2__, typename T3__>
-    Eigen::Matrix<stan::promote_args_t<value_type_t<T0__>, value_type_t<T1__>,
-    value_type_t<T2__>,
+    Eigen::Matrix<stan::promote_args_t<stan::value_type_t<T0__>, stan::value_type_t<T1__>,
+    stan::value_type_t<T2__>,
     T3__>, -1, -1>
     sars(const T0__& x_arg__, const T1__& y_arg__, const T2__& z_arg__,
          const std::vector<Eigen::Matrix<T3__, -1, -1>>& w,
          std::ostream* pstream__) {
-      using local_scalar_t__ = stan::promote_args_t<value_type_t<T0__>,
-              value_type_t<T1__>,
-              value_type_t<T2__>,
+      using local_scalar_t__ = stan::promote_args_t<stan::value_type_t<T0__>,
+              stan::value_type_t<T1__>,
+              stan::value_type_t<T2__>,
               T3__>;
       const auto& x = to_ref(x_arg__);const auto& y = to_ref(y_arg__);
       const auto& z = to_ref(z_arg__);
@@ -104,8 +108,8 @@ let%expect_test "udf-expressions" =
 
     struct sars_functor__ {
     template <typename T0__, typename T1__, typename T2__, typename T3__>
-    Eigen::Matrix<stan::promote_args_t<value_type_t<T0__>, value_type_t<T1__>,
-    value_type_t<T2__>,
+    Eigen::Matrix<stan::promote_args_t<stan::value_type_t<T0__>, stan::value_type_t<T1__>,
+    stan::value_type_t<T2__>,
     T3__>, -1, -1>
     operator()(const T0__& x, const T1__& y, const T2__& z,
                const std::vector<Eigen::Matrix<T3__, -1, -1>>& w,
