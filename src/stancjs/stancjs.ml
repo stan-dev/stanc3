381e796137b0b1c31aa8314a1533888f76536623
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-12-09 20:49:23 +0100

    format

--- src/stancjs/stancjs.ml
+++ src/stancjs/stancjs.ml
@@ -54,7 +54,7 @@ let stan2cpp model_name model_string flags =
     |> Result.map ~f:(fun typed_ast ->
            if is_flag_set "print-canonical" then
              Pretty_printing.pretty_print_typed_program
-                 (Canonicalize.canonicalize_program typed_ast)
+               (Canonicalize.canonicalize_program typed_ast)
            else if is_flag_set "auto-format" then
              match ast with
              | Result.Ok f -> Pretty_printing.pretty_print_program f
@@ -71,7 +71,7 @@ let stan2cpp model_name model_string flags =
                Pedantic_analysis.print_warn_uninitialized mir ;
              if is_flag_set "warn-pedantic" then
                Pedantic_analysis.print_warn_pedantic mir ;
-             cpp)
+             cpp )
 
 let stanc_stdout = ref ""
 let stanc_stderr = ref ""
@@ -80,19 +80,17 @@ let wrap_result = function
   | Result.Ok s ->
       Js.Unsafe.obj
         [| ("result", Js.Unsafe.inject (Js.string s))
-         ; ( "warnings"
-           , Js.Unsafe.inject (Js.string !stanc_stderr)
-               ) |]
+         ; ("warnings", Js.Unsafe.inject (Js.string !stanc_stderr)) |]
   | Error e ->
       Js.Unsafe.obj
         [| ("errors", Js.Unsafe.inject (Array.map ~f:Js.string [|e|]))
          ; ("warnings", Js.Unsafe.inject (Js.string !stanc_stderr)) |]
 
 let stan2cpp_wrapped name code (flags : Js.string_array Js.t Js.opt) =
-  stanc_stdout := "";
-  stanc_stderr := "";
-  Sys_js.set_channel_flusher stdout (fun s -> (stanc_stdout := !stanc_stdout ^ s));
-  Sys_js.set_channel_flusher stderr (fun s -> (stanc_stderr := !stanc_stderr ^ s));
+  stanc_stdout := "" ;
+  stanc_stderr := "" ;
+  Sys_js.set_channel_flusher stdout (fun s -> stanc_stdout := !stanc_stdout ^ s) ;
+  Sys_js.set_channel_flusher stderr (fun s -> stanc_stderr := !stanc_stderr ^ s) ;
   stan2cpp (Js.to_string name) (Js.to_string code)
     Js.(
       Opt.map flags (fun a ->
