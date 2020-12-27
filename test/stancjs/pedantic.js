485efc55ea28627aaf0887648682b6dcc0c81329
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-12-27 16:46:22 +0100

    wrapped remaining warning prints in JSON.stringify

--- test/stancjs/pedantic.js
+++ test/stancjs/pedantic.js
@@ -12,12 +12,12 @@ model {
 `
 var pedantic_test = stanc.stanc("pedantic", pedantic_model, ["warn-pedantic"]);
 if (pedantic_test.warnings) {
-    console.log(pedantic_test.warnings)
+    console.log(JSON.stringify(pedantic_test.warnings))
 }
 
 var pedantic_test = stanc.stanc("pedantic", pedantic_model);
 if (pedantic_test.warnings) {
-    console.log(pedantic_test.warnings)
+    console.log(JSON.stringify(pedantic_test.warnings))
 }
 var warn_uninit_model = `
 transformed data { 
