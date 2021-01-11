346eb45d52ee72fb9b4da182fc42e08cfc91bc66
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-11 18:24:25 +0100

    add svd_U and svd_V

--- src/middle/Stan_math_signatures.ml
+++ src/middle/Stan_math_signatures.ml
@@ -1700,6 +1700,8 @@ let () =
   add_unqualified ("sum", ReturnType UReal, [UVector]) ;
   add_unqualified ("sum", ReturnType UReal, [URowVector]) ;
   add_unqualified ("sum", ReturnType UReal, [UMatrix]) ;
+  add_unqualified ("svd_U", ReturnType UMatrix, [UMatrix]) ;
+  add_unqualified ("svd_V", ReturnType UMatrix, [UMatrix]) ;
   add_unqualified ("tail", ReturnType URowVector, [URowVector; UInt]) ;
   add_unqualified ("tail", ReturnType UVector, [UVector; UInt]) ;
   List.iter
