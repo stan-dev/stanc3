9a71c2fb0643416994c1ff928947cfa8f20578e9
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-14 20:25:16 +0100

    format

--- src/frontend/Pretty_printing.ml
+++ src/frontend/Pretty_printing.ml
@@ -338,7 +338,7 @@ and pp_statement ppf ({stmt= s_content; _} as ss) =
       Format.pp_print_cut ppf () ;
       Fmt.pf ppf "}"
   | Profile (name, vdsl) ->
-      Fmt.pf ppf "profile(%s) {" name;
+      Fmt.pf ppf "profile(%s) {" name ;
       Format.pp_print_cut ppf () ;
       with_indented_box ppf 2 0 (fun () -> pp_list_of_statements ppf vdsl) ;
       Format.pp_print_cut ppf () ;
