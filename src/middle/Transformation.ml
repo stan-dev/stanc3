100f3533972190c153929e6e80c27637bf1c5858
Ryan Bernstein <ryanbernstein1@gmail.com>
2021-04-23 22:52:01 -0400

    Adding Transformation module to avoid cyclic dependency

---
+++ src/middle/Transformation.ml
@@ -0,0 +1,22 @@
+(** Transformations (constraints) for global variable declarations *)
+type 'e t =
+  | Identity
+  | Lower of 'e
+  | Upper of 'e
+  | LowerUpper of 'e * 'e
+  | Offset of 'e
+  | Multiplier of 'e
+  | OffsetMultiplier of 'e * 'e
+  | Ordered
+  | PositiveOrdered
+  | Simplex
+  | UnitVector
+  | CholeskyCorr
+  | CholeskyCov
+  | Correlation
+  | Covariance
+[@@deriving sexp, compare, map, hash, fold]
+
+let has_check = function
+  | Identity | Offset _ | Multiplier _ | OffsetMultiplier _ -> false
+  | _ -> true
