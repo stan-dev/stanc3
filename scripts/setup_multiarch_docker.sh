c092edf9f27fcd9257b0385c80aab6a497bcf84b
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-19 20:40:05 +0800

    Access correct dir

--- scripts/setup_multiarch_docker.sh
+++ scripts/setup_multiarch_docker.sh
@@ -1,14 +1,14 @@
-apt update;
-apt install opam bzip2 git tar curl ca-certificates openssl m4 bash -y;
+apt-get update
+apt-get install opam bzip2 git tar curl ca-certificates openssl m4 bash -y
 
-opam init --disable-sandboxing -y;
-opam switch create 4.07.0;
-opam switch 4.07.0;
-eval $(opam env);
-opam repo add internet https://opam.ocaml.org;
+opam init --disable-sandboxing -y
+opam switch create 4.07.0
+opam switch 4.07.0
+eval $(opam env)
+opam repo add internet https://opam.ocaml.org
 
 cd /home/jenkins-slave/jenkins-slave-files/workspace
-cd */
-bash -x scripts/install_build_deps.sh;
-dune build @install --profile static;
+cd $(ls | grep -v @tmp)
+bash -x scripts/install_build_deps.sh
+dune build @install --profile static
 exit
