819a5e04804b1e0bbffa5d57da9959a12dda8c39
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-27 13:06:35 +0800

    Test permissions

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -4,11 +4,11 @@ mount --bind $(pwd) /var/chroot/$1/$(pwd)
 chroot /var/chroot/$1/ /bin/bash << EOF
   cd $(pwd)
 
-  eval \$(opam env)
+  eval \\\$(opam env)
   dune build @install --profile static
 EOF
 " > scripts/build_stanc3.sh
 
-docker run --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:$1-debootstrap /bin/bash -x $(pwd)/scripts/build_stanc3.sh
+docker run --privileged --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:$1-debootstrap /bin/bash -x $(pwd)/scripts/build_stanc3.sh
 
 rm scripts/build_stanc3.sh
