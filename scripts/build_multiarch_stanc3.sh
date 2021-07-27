2c12b9a8f90df4d3b786fbbc77dc6e151e62a143
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-27 12:58:45 +0800

    Alternative bash call

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -4,6 +4,6 @@ chroot /var/chroot/$1/ /bin/bash -e \"cd $(pwd) && eval \\\$(opam env) && dune b
 
 mkdir -p /var/chroot/$1/$(pwd)
 
-docker run --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:$1-debootstrap /bin/bash -x $(pwd)/scripts/build_stanc3.sh
+docker run --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:$1-debootstrap /bin/bash -c "/bin/bash -x $(pwd)/scripts/build_stanc3.sh"
 
 rm scripts/build_stanc3.sh
