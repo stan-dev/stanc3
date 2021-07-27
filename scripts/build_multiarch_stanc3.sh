e4698c7743f9c22829e777b4b5d4f90551fb8726
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-27 12:43:08 +0800

    Update build scrit

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -1,14 +1,8 @@
 echo "
-chroot /var/chroot/$1/ /bin/bash << EOF
-  cd $(pwd)
-
-  eval \$(opam env)
-  dune build @install --profile static
-EOF
+chroot /var/chroot/$1/ /bin/bash -c \"cd $(pwd) && eval \$(opam env) && dune build @install --profile static\"
 " > scripts/build_stanc3.sh
 
 mkdir -p /var/chroot/$1/$(pwd)
-mount --bind $(pwd) /var/chroot/$1/$(pwd)
 
 docker run --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:$1-debootstrap /bin/bash -x $(pwd)/scripts/build_stanc3.sh
 
