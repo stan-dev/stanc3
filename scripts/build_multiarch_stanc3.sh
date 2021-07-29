eca42939ca556094beaa5e769f2e433a427c0b04
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-29 11:04:57 +0800

    Revert to sha256 tags for now

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -1,19 +1,18 @@
 # Identify 
 if [ $1 = "mips64el" ]; then
-  ARCH="linux/mips64le"
+  SHA="sha256:85cc1b0be45320410ea10e607c7db8d90f6448945b0e27675f3ea7150245d618"
 elif [ $1 = "arm64" ]; then
-  ARCH="linux/arm64"
+  SHA="sha256:241805782f6acc79d623f4d0bbf09138bf700e2b1ff37fd7527b8411a932ea9c"
 elif [ $1 = "ppc64el" ]; then
-  ARCH="linux/ppc64le"
+  SHA="sha256:52a733211ee03875fc7be1e16853d7752b8d2b39a239bce76a54d98eb58083cb"
 elif [ $1 = "armhf" ]; then
-  ARCH="linux/arm/v7"
+  SHA="sha256:0fa3787e3fb8a0ef6d1a11cb388c0ed8ad3c8a196d680021d089de167db5c1c4"
 elif [ $1 = "armel" ]; then
-  ARCH="linux/arm/v6"
+  SHA="sha256:e128ba728675e89404383b82b487f639baa70f0d25c0d1db8c6eb34f9a23b920"
 else
-  ARCH=$1
+  SHA=$1
 fi
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker run --volumes-from=$(docker ps -q):rw --platform $ARCH andrjohns/stanc3-building:latest /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
+docker run --volumes-from=$(docker ps -q):rw --platform andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 
