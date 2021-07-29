8b60333d363108ef54e1216d575ac1ea2bb65113
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-29 12:33:38 +0800

    Test new binfmt call

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -22,5 +22,6 @@ docker create --name dummy multiarch/qemu-user-static:x86_64-$ARCH bash
 docker cp dummy:/usr/bin/qemu-$ARCH-static qemu-$ARCH-static
 docker rm -f dummy
 
+docker run --rm --privileged multiarch/qemu-user-static --reset
 docker run --volumes-from=$(docker ps -q):rw -v $(pwd)/qemu-$ARCH-static:/usr/bin/qemu-$ARCH-static andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 
