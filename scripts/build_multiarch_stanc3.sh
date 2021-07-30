7b3f5355ec280d0bd7fadb44777978cbd1952fee
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-30 09:58:20 +0800

    Improve doc & tidy

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -1,4 +1,4 @@
-# Identify 
+# Identify the sha256 tag of desired docker image architecture and name of QEMU architecture
 if [ $1 = "mips64el" ]; then
   SHA="sha256:f674ab3524ccdc9af392e50c3fa0be937c0bb8df54be1b4b671460197d3efebd"
   ARCH="mips64el"
@@ -21,10 +21,18 @@ else
   SHA=$1
 fi
 
+# Download QEMU translation binary for desired architecture
 docker create --name dummy multiarch/qemu-user-static:x86_64-$ARCH bash
 docker cp dummy:/usr/bin/qemu-$ARCH-static qemu-$ARCH-static
 docker rm -f dummy
 
+# Register QEMU translation binaries
 docker run --rm --privileged multiarch/qemu-user-static --reset
-docker run --volumes-from=$(docker ps -q):rw -v $(pwd)/qemu-$ARCH-static:/usr/bin/qemu-$ARCH-static andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
+
+# Run docker, inheriting mounted volumes from sibling container (including stanc3 directory)
+docker run --volumes-from=$(docker ps -q):rw \
+           # Mount downloaded QEMU binary in container
+           -v $(pwd)/qemu-$ARCH-static:/usr/bin/qemu-$ARCH-static \
+           andrjohns/stanc3-building:latest@$SHA \
+           /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 
