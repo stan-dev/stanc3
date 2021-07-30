f8676b023e66098088cdfabb050490fdc2dfa53b
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-30 08:18:36 +0800

    Use debian stable

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -5,7 +5,6 @@ if [ $1 = "mips64el" ]; then
 elif [ $1 = "arm64" ]; then
   SHA="sha256:d9311875f5fcd5f9b2221efb9d8a10ea8a159ca0e30cc60f04a0333ba14a4371"
   ARCH="aarch64"
-  FLAGS="export CFLAGS=\"\$CFLAGS -fPIC\" &&"
 elif [ $1 = "ppc64el" ]; then
   SHA="sha256:cce409af4067aa8f6825dc07c04a7038c025a4a9b7b71355ce04e066f2c65b07"
   ARCH="ppc64le"
@@ -20,7 +19,6 @@ elif [ $1 = "s390x" ]; then
   ARCH="s390x"
 else
   SHA=$1
-  FLAGS=""
 fi
 
 docker create --name dummy multiarch/qemu-user-static:x86_64-$ARCH bash
@@ -28,5 +26,5 @@ docker cp dummy:/usr/bin/qemu-$ARCH-static qemu-$ARCH-static
 docker rm -f dummy
 
 docker run --rm --privileged multiarch/qemu-user-static --reset
-docker run --volumes-from=$(docker ps -q):rw -v $(pwd)/qemu-$ARCH-static:/usr/bin/qemu-$ARCH-static andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && $FLAGS dune build @install --profile static"
+docker run --volumes-from=$(docker ps -q):rw -v $(pwd)/qemu-$ARCH-static:/usr/bin/qemu-$ARCH-static andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 
