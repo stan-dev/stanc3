b1feea4f81640820d1133b20487999cb24ad62e3
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-08-01 11:40:16 +0800

    Add doc, put qemu binary in docker image

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -1,49 +1,42 @@
-# Identify the sha256 tag of desired docker image architecture and name of QEMU architecture
+# Architecture naming isn't consistent between QEMU and Docker, so lookup correct naming
 if [ $1 = "mips64el" ]; then
   export DOCK_ARCH="mips64le"
   export DOCK_VARIANT=""
-  QEMU_ARCH="mips64el"
 elif [ $1 = "arm64" ]; then
   export DOCK_ARCH="arm64"
   export DOCK_VARIANT=""
-  QEMU_ARCH="aarch64"
 elif [ $1 = "ppc64el" ]; then
   export DOCK_ARCH="ppc64le"
   export DOCK_VARIANT=""
-  QEMU_ARCH="ppc64le"
 elif [ $1 = "armhf" ]; then
   export DOCK_ARCH="arm"
   export DOCK_VARIANT="v7"
-  QEMU_ARCH="arm"
 elif [ $1 = "armel" ]; then
   export DOCK_ARCH="arm"
   export DOCK_VARIANT="v6"
-  QEMU_ARCH="arm"
 elif [ $1 = "s390x" ]; then
   export DOCK_ARCH="s390x"
   export DOCK_VARIANT=""
-  QEMU_ARCH="s390x"
 fi
 
+# Need to use the sha256 tag to force docker to download an architecture that differs from the host
+# We'll use the skopeo package to lookup the most recent tag, but need to manually install a more recent version
 curl https://dl-cdn.alpinelinux.org/alpine/latest-stable/community/x86_64/containers-common-0.38.11-r0.apk -o cont.apk
 curl https://dl-cdn.alpinelinux.org/alpine/latest-stable/community/x86_64/skopeo-1.3.1-r0.apk -o skopeo.apk
 
 apk add cont.apk
 apk add skopeo.apk
 
+# Lookup the sha256 hash for the specified architecture and variant (e.g., v7 for armhf) and strip the enclosing quotations
 SHA=$(skopeo inspect --raw docker://andrjohns/stanc3-building:latest | jq '.manifests | .[] | select(.platform.architecture==env.DOCK_ARCH and .platform.variant==(if env.DOCK_VARIANT == "" then null else env.DOCK_VARIANT end)).digest' | tr -d '"')
-
-# Download QEMU translation binary for desired architecture
-docker create --name dummy multiarch/qemu-user-static:x86_64-$QEMU_ARCH bash
-docker cp dummy:/usr/bin/qemu-$QEMU_ARCH-static qemu-$QEMU_ARCH-static
-docker rm -f dummy
 
 # Register QEMU translation binaries
 docker run --rm --privileged multiarch/qemu-user-static --reset
 
-# Run docker, inheriting mounted volumes from sibling container (including stanc3 directory)
-docker run --volumes-from=$(docker ps -q):rw -v $(pwd)/qemu-$QEMU_ARCH-static:/usr/bin/qemu-$QEMU_ARCH-static andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
+# Run docker, inheriting mounted volumes from sibling container (including stanc3 directory), and build stanc3
+docker run --volumes-from=$(docker ps -q):rw andrjohns/stanc3-building:latest@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 
+# Update ownership of build folders
 chown -R opam: _build
 chown -R opam: src
 chown -R opam: test
