af1c2793ee6392b09805e283864b5e5464459e9a
Adam Haber <adamhaber@gmail.com>
2021-10-14 09:35:36 +0300

    turn single-statement branches into blocks

--- src/frontend/Canonicalize.ml
+++ src/frontend/Canonicalize.ml
@@ -144,7 +144,15 @@ and keep_parens {expr; emeta} =
 
 let parens_lval = map_lval_with no_parens ident
 
-let rec parens_stmt {stmt; smeta} =
+let stmt_to_block ({stmt; smeta} : typed_statement) : typed_statement =
+  match stmt with
+  | Block _ -> {stmt; smeta}
+  | _ ->
+      mk_typed_statement
+        ~stmt:(Block [{stmt; smeta}])
+        ~return_type:smeta.return_type ~loc:smeta.loc
+
+let rec parens_stmt ({stmt; smeta} : typed_statement) : typed_statement =
   let stmt =
     match stmt with
     | VarDecl
@@ -159,12 +167,17 @@ let rec parens_stmt {stmt; smeta} =
           ; identifier
           ; initial_value= Option.map ~f:no_parens init
           ; is_global }
+    | While (e, s) -> While (e, stmt_to_block s)
+    | IfThenElse (e, s1, s2) -> (
+      match s2 with
+      | Some x -> IfThenElse (e, stmt_to_block s1, Some (stmt_to_block x))
+      | None -> IfThenElse (e, stmt_to_block s1, None) )
     | For {loop_variable; lower_bound; upper_bound; loop_body} ->
         For
           { loop_variable
           ; lower_bound= keep_parens lower_bound
           ; upper_bound= keep_parens upper_bound
-          ; loop_body= parens_stmt loop_body }
+          ; loop_body= parens_stmt (stmt_to_block loop_body) }
     | _ -> map_statement no_parens parens_stmt parens_lval ident stmt
   in
   {stmt; smeta}
