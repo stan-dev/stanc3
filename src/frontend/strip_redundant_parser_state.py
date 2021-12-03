fe19284f23045d4dee11413e82f9928a71b49aff
Brian Ward <bward@flatironinstitute.org>
2021-12-03 11:45:44 -0500

    Keep all concrete syntax

--- src/frontend/strip_redundant_parser_state.py
+++ src/frontend/strip_redundant_parser_state.py
@@ -2,13 +2,26 @@
 import re
 import sys
 
+
+matcher = re.compile(
+    r"((program|functions_only):.*\n)"
+    r"(##.*\n)"
+    r"(## Concrete.*\n)?"
+    r"(##.*\n)+"
+    r"((program|functions_only):.*\n)"
+)
+
+
 def strip_redundant_parser_states(s):
-    return re.sub("(program:.*\n)(##.*\n)+(program:.*\n)",
-                  lambda m: m.group(1) + m.group(3),
-                  s)
+    return matcher.sub(
+        lambda m: m.group(1) + m.group(4) + m.group(6),
+        s,
+    )
+
 
 def strip_lines(s):
     return "\n".join(map(lambda s: s.rstrip(), s.splitlines()))
+
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
