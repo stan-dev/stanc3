c4cb2893a183e7887967a889f6ef60d15ef74f51
Niko Huurre <nhuurre@protonmail.com>
2022-01-10 17:08:59 +0200

    mention canonicalize flag

--- src/frontend/Typechecker.ml
+++ src/frontend/Typechecker.ml
@@ -778,7 +778,9 @@ let check_lvalue cf tenv = function
         add_warning loc
           "Nested multi-indexing on the left hand side of assignment does not \
            behave the same as nested indexing in expressions. This is \
-           considered a bug and will be disallowed in Stan 2.32.0." ;
+           considered a bug and will be disallowed in Stan 2.32.0. The \
+           indexing can be automatically fixed using the canonicalize flag for \
+           stanc." ;
         let lvalue_rvalue_types_differ =
           try
             let flat_type =
