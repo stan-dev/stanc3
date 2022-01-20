3987eac55d97f2969a40ae2afcdb164c14c3d643
Nicusor Serban <serban.nicusor@toptal.com>
2022-01-21 00:42:35 +0100

    Debug

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -26,4 +26,13 @@ SHA=$(skopeo inspect --raw docker://stanorg/stanc3:multiarchfi | jq '.manifests 
 docker run --rm --privileged multiarch/qemu-user-static --reset
 
 # Run docker, inheriting mounted volumes from sibling container (including stanc3 directory), and build stanc3 #
-docker run -u 990:986 --group-add=987 --group-add=988 stanorg/stanc3:multiarchfi@$SHA /bin/bash -c "eval \$(opam env) && dune build @install --profile static"
+# docker run -u 990:986 --group-add=987 --group-add=988 --volumes-from=$(docker ps -qf "ancestor=stanorg/stanc3:staticfi"):rw stanorg/stanc3:multiarchfi@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
+docker run -u 990:986 --group-add=987 --group-add=988 --volumes-from=$(docker ps -qf "ancestor=stanorg/stanc3:staticfi"):rw stanorg/stanc3:multiarchfi@$SHA /bin/bash -c "cd $(pwd) && ls -lhart && whoami $&& echo ${UID} && getent group sudo"
+
+# Update ownership of build folders
+#chown -R opam: _build
+#chown -R opam: src
+#chown -R opam: test
+
+# docker buildx build -t stanorg/stanc3:multiarchfi --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/mips64le,linux/s390x --no-cache --build-arg PUID=990 --progress=plain --build-arg PGID=986 --push .
+# docker run -v $(pwd):/home/jenkins stanorg/stanc3:multiarchfi@sha256:f5098ad99b4245da5f811e329af326bf241e46c67789e16906da7364b3fb9db1 /bin/bash -c 'which opam && eval $(opam env) && dune build @install --profile static'
