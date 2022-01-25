20172ab890ddeb1f57ddd813e7af5252c02d09e4
Nicusor Serban <serban.nicusor@toptal.com>
2022-01-25 15:12:37 +0100

    Debug

--- scripts/build_multiarch_stanc3.sh
+++ scripts/build_multiarch_stanc3.sh
@@ -34,7 +34,8 @@ pwd
 # Run docker, inheriting mounted volumes from sibling container (including stanc3 directory), and build stanc3 #
 # docker run -u 990:986 --group-add=987 --group-add=988 --volumes-from=$(docker ps -qf "ancestor=stanorg/stanc3:staticfi"):rw stanorg/stanc3:multiarchfi@$SHA /bin/bash -c "cd $(pwd) && eval \$(opam env) && dune build @install --profile static"
 # docker run -t -d -u 990:986 --entrypoint= -w /home/jenkins/workspace/Stan_Stanc3_PR-1087 -v /home/jenkins/workspace/Stan_Stanc3_PR-1087:/home/jenkins/workspace/Stan_Stanc3_PR-1087:rw,z -v /home/jenkins/workspace/Stan_Stanc3_PR-1087@tmp:/home/jenkins/workspace/Stan_Stanc3_PR-1087@tmp:rw,z
-docker run -w $(pwd) -v $(pwd):$(pwd):rw stanorg/stanc3:multiarchfi@$SHA printenv
+# -w $(pwd) -v $(pwd):$(pwd):rw
+docker run stanorg/stanc3:multiarchfi@$SHA printenv
 
 # Update ownership of build folders
 #chown -R opam: _build
