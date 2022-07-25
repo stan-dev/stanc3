1a0af6793f7b465363313fa0142f4924d764bcc8
Brian Ward <bward@flatironinstitute.org>
2022-07-25 12:49:43 -0400

    Swap order of variable renames during inlining to avoid outside name captures

--- src/analysis_and_optimization/Optimize.ml
+++ src/analysis_and_optimization/Optimize.ml
@@ -287,11 +287,8 @@ let rec inline_function_expression propto adt fim (Expr.Fixed.{pattern; _} as e)
                      replaced to avoid conflict with the (two) new dummy
                      variables introduced by inlining *)
                 , [ handle
-                      (replace_fresh_local_vars fname
-                         (subst_args_stmt args es body) ) ]
-                  (* [ handle
-                        (subst_args_stmt args es
-                           (replace_fresh_local_vars fname body) ) ] *)
+                      (subst_args_stmt args es
+                         (replace_fresh_local_vars fname body) ) ]
                 , { pattern= Var inline_return_name
                   ; meta=
                       Expr.Typed.Meta.
