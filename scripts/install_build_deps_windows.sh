aa3a3bdd9cbeee9279f90f4534a01214e881eea1
Brian Ward <bward@flatironinstitute.org>
2024-02-16 14:12:56 -0500

    Update install_build_deps_windows.sh

--- scripts/install_build_deps_windows.sh
+++ scripts/install_build_deps_windows.sh
@@ -17,6 +17,7 @@ opam install -y conf-flambda-windows
 opam install -y "ocaml-windows64=4.14.1"
 
 # Install dependencies
-opam install -y core.v0.16.1 core-windows.v0.16.1 menhir.20230608 menhir-windows.20230608 ppx_deriving.5.2.1 ppx_deriving-windows.5.2.1 fmt.0.9.0 fmt-windows.0.9.0 yojson.2.1.0 yojson-windows.2.1.0 uucp.15.1.0 uucp-windows.15.1.0 uunf.15.1.0 uunf-windows.15.1.0
+opam install -y core.v0.16.1 core-windows.v0.16.1 menhir.20230608 menhir-windows.20230608 ppx_deriving.5.2.1 ppx_deriving-windows.5.2.1 fmt.0.9.0 fmt-windows.0.9.0 yojson.2.1.0 yojson-windows.2.1.0
+opam install -y uucp.15.1.0 uucp-windows.15.1.0 uunf.15.1.0 uunf-windows.15.1.0
 
 eval $(opam env)
