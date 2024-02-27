dcb893cf107f1a152eef591ec24e3b39d57dffe8
Nicusor Serban <nserban@dymeng.com>
2024-02-27 18:01:27 +0100

    Downgrade core to v0.16.0

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -5,8 +5,8 @@ set -e
 
 eval $(opam env)
 
-opam pin -y core v0.16.1 --no-action
+opam pin -y core v0.16.0 --no-action
 
-opam install -y dune core.v0.16.1 menhir.20230608 ppx_deriving.5.2.1 fmt.0.9.0 yojson.2.1.0
+opam install -y dune core.v0.16.0 menhir.20230608 ppx_deriving.5.2.1 fmt.0.9.0 yojson.2.1.0
 
 eval $(opam env)
