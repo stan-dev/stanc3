9e339af462fe97256f74b27cba0625944d2f37c9
Nicusor Serban <nserban@dymeng.com>
2024-02-28 15:47:05 +0100

    Fix build deps

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -5,7 +5,7 @@ set -e
 
 eval $(opam env)
 
-opam pin -y core v0.16.0 --no-action
+opam pin -y core v0.16.1 --no-action
 
 opam install -y dune core.v0.16.1 menhir.20230608 ppx_deriving.5.2.1 fmt.0.9.0 yojson.2.1.0
 
