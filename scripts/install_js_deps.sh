8dd4fa9d496a7d2cd9d0f54df200d28a014ed221
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-22 00:22:15 +0300

    Add links to issues

--- scripts/install_js_deps.sh
+++ scripts/install_js_deps.sh
@@ -5,6 +5,8 @@ set -e
 
 # JSOO versions higher than this break QuickJS (labelled break statements)
 # Please check compatibility before any updates!
+# https://github.com/stan-dev/stanc3/issues/1425
+# https://github.com/bellard/quickjs/issues/275
 opam pin -y js_of_ocaml 5.4.0
 
 echo "You should also install node in order to run the tests."
