bf1f0db04f9772a43d527f6cb2a3a89ae6e9b422
Brian Ward <bward@flatironinstitute.org>
2024-06-21 14:27:23 -0400

    More tests

--- test/stancjs/includes.js
+++ test/stancjs/includes.js
@@ -14,19 +14,6 @@ var bar_includes = {"bar.stan":"// nothing here"};
 var include_test_missing = stanc.stanc("include-testtest", include_model, [], bar_includes);
 utils.print_error(include_test_missing)
 
-
-var include_test_bad = stanc.stanc("empty", "model {}", [], {"foo.stan": {"internal":"that wasn't a string!"}});
-utils.print_error(include_test_bad)
-utils.print_warnings(include_test_bad)
-
-var include_test_bad = stanc.stanc("empty", "model {}", [], {"foo.stan": "1", "./foo.stan": "2"});
-utils.print_error(include_test_bad)
-utils.print_warnings(include_test_bad)
-
-var include_test_bad = stanc.stanc("empty", "model {}", [], 1234);
-utils.print_error(include_test_bad)
-utils.print_warnings(include_test_bad)
-
 // good
 var foo_code = `
 functions {
@@ -45,3 +32,25 @@ utils.print_result(include_format_test)
 var include_info_test = stanc.stanc("include-testtest", include_model, ["info"], {...bar_includes, "foo.stan":foo_code});
 utils.print_error(include_info_test)
 utils.print_result(include_info_test)
+
+
+// warnings
+var include_test_bad = stanc.stanc("empty", "model {}", [], {"foo.stan": {"internal":"that wasn't a string!"}});
+utils.print_error(include_test_bad)
+utils.print_warnings(include_test_bad)
+
+var include_test_bad = stanc.stanc("empty", "model {}", [], {"foo.stan": "1", "./foo.stan": "2"});
+utils.print_error(include_test_bad)
+utils.print_warnings(include_test_bad)
+
+var include_test_bad = stanc.stanc("empty", "model {}", [], 1234);
+utils.print_error(include_test_bad)
+utils.print_warnings(include_test_bad)
+
+// other errors: recursive includes
+var recursive_a = `#include <include/a.stan>`
+var recursive_b = `#include <include/b.stan>`
+
+var recursive_test = stanc.stanc("recursive", recursive_a, [], {"include/a.stan":recursive_b, "include/b.stan":recursive_a});
+utils.print_error(recursive_test)
+utils.print_warnings(recursive_test)
