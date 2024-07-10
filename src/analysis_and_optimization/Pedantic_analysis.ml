140d6860e23934b7de4a648bce65e885137c25a6
Brian Ward <bward@flatironinstitute.org>
2024-07-10 09:19:50 -0400

    Update pedantic mode warning for jacobian usage

--- src/analysis_and_optimization/Pedantic_analysis.ml
+++ src/analysis_and_optimization/Pedantic_analysis.ml
@@ -391,7 +391,9 @@ let maybe_jacobian_adjustment_warnings (mir : Program.Typed.t) =
       , "Left-hand side of distribution statement (~) may contain a non-linear \
          transform of a parameter or local variable. If it does, you need to \
          include a target += statement with the log absolute determinant of \
-         the Jacobian of the transform." ))
+         the Jacobian of the transform. You could also consider defining a \
+         transformed parameter and using jacobian += in the transformed \
+         parameters block." ))
     locations
 
 let multi_tildes_message (vname : string) : string =
