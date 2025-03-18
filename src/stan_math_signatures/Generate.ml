84c5336baa531ef99073f90e8faba71ae4e6ee54
Brian Ward <bward@flatironinstitute.org>
2025-03-18 14:57:44 -0400

    Helper function to create unary vector overloads

--- src/stan_math_signatures/Generate.ml
+++ src/stan_math_signatures/Generate.ml
@@ -580,6 +580,14 @@ let add_first_arg_vector_ternary name supports_soa =
         vectors)
     (List.range 0 8)
 
+(** For functions that accept and return (arrays of...) vectors*)
+let add_nested_vector_unary name supports_soa =
+  List.iter
+    ~f:(fun i ->
+      let t = bare_array_type (UVector, i) in
+      add_unqualified (name, ReturnType t, [t], supports_soa))
+    (List.range 0 8)
+
 let add_ternary name supports_soa =
   add_unqualified (name, ReturnType UReal, [UReal; UReal; UReal], supports_soa)
 
@@ -1910,13 +1918,9 @@ let () =
   add_unqualified ("ones_array", ReturnType (UArray UReal), [UInt], SoA);
   add_unqualified ("ones_row_vector", ReturnType URowVector, [UInt], SoA);
   add_unqualified ("ones_vector", ReturnType UVector, [UInt], SoA);
-  List.iter
-    ~f:(fun i ->
-      let t = bare_array_type (UVector, i) in
-      add_unqualified ("ordered_jacobian", ReturnType t, [t], SoA);
-      add_unqualified ("ordered_constrain", ReturnType t, [t], SoA);
-      add_unqualified ("ordered_unconstrain", ReturnType t, [t], AoS))
-    (List.range 0 8);
+  add_nested_vector_unary "ordered_jacobian" SoA;
+  add_nested_vector_unary "ordered_constrain" SoA;
+  add_nested_vector_unary "ordered_unconstrain" AoS;
   add_unqualified
     ( "ordered_logistic_glm_lpmf"
     , ReturnType UReal
@@ -1987,13 +1991,9 @@ let () =
     , SoA );
   add_unqualified ("polar", ReturnType UComplex, [UReal; UReal], AoS);
   add_nullary "positive_infinity";
-  List.iter
-    ~f:(fun i ->
-      let t = bare_array_type (UVector, i) in
-      add_unqualified ("positive_ordered_jacobian", ReturnType t, [t], SoA);
-      add_unqualified ("positive_ordered_constrain", ReturnType t, [t], SoA);
-      add_unqualified ("positive_ordered_unconstrain", ReturnType t, [t], AoS))
-    (List.range 0 8);
+  add_nested_vector_unary "positive_ordered_jacobian" SoA;
+  add_nested_vector_unary "positive_ordered_constrain" SoA;
+  add_nested_vector_unary "positive_ordered_unconstrain" AoS;
   add_binary_vec "pow" AoS;
   add_binary_vec_complex_complex "pow" AoS;
   add_unqualified ("prod", ReturnType UInt, [UArray UInt], AoS);
@@ -2213,13 +2213,9 @@ let () =
             , SoA ))
         (List.range 1 4))
     bare_types;
-  List.iter
-    ~f:(fun i ->
-      let t = bare_array_type (UVector, i) in
-      add_unqualified ("simplex_jacobian", ReturnType t, [t], SoA);
-      add_unqualified ("simplex_constrain", ReturnType t, [t], SoA);
-      add_unqualified ("simplex_unconstrain", ReturnType t, [t], AoS))
-    (List.range 0 8);
+  add_nested_vector_unary "simplex_jacobian" SoA;
+  add_nested_vector_unary "simplex_constrain" SoA;
+  add_nested_vector_unary "simplex_unconstrain" AoS;
   add_unqualified ("sin", ReturnType UComplex, [UComplex], AoS);
   add_unqualified ("sinh", ReturnType UComplex, [UComplex], AoS);
   add_unqualified ("singular_values", ReturnType UVector, [UMatrix], SoA);
@@ -2328,13 +2324,9 @@ let () =
   add_unqualified ("sum", ReturnType UComplex, [UComplexVector], SoA);
   add_unqualified ("sum", ReturnType UComplex, [UComplexRowVector], SoA);
   add_unqualified ("sum", ReturnType UComplex, [UComplexMatrix], SoA);
-  List.iter
-    ~f:(fun i ->
-      let t = bare_array_type (UVector, i) in
-      add_unqualified ("sum_to_zero_jacobian", ReturnType t, [t], SoA);
-      add_unqualified ("sum_to_zero_constrain", ReturnType t, [t], SoA);
-      add_unqualified ("sum_to_zero_unconstrain", ReturnType t, [t], AoS))
-    (List.range 0 8);
+  add_nested_vector_unary "sum_to_zero_jacobian" SoA;
+  add_nested_vector_unary "sum_to_zero_constrain" SoA;
+  add_nested_vector_unary "sum_to_zero_unconstrain" AoS;
   (* TODO (future): SoA inside of tuples, update following signatures *)
   add_unqualified
     ("svd", ReturnType (UTuple [UMatrix; UVector; UMatrix]), [UMatrix], AoS);
@@ -2524,13 +2516,9 @@ let () =
     ("transpose", ReturnType UComplexVector, [UComplexRowVector], SoA);
   add_unqualified ("transpose", ReturnType UComplexMatrix, [UComplexMatrix], SoA);
   add_unqualified ("uniform_simplex", ReturnType UVector, [UInt], SoA);
-  List.iter
-    ~f:(fun i ->
-      let t = bare_array_type (UVector, i) in
-      add_unqualified ("unit_vector_jacobian", ReturnType t, [t], SoA);
-      add_unqualified ("unit_vector_constrain", ReturnType t, [t], SoA);
-      add_unqualified ("unit_vector_unconstrain", ReturnType t, [t], AoS))
-    (List.range 0 8);
+  add_nested_vector_unary "unit_vector_jacobian" SoA;
+  add_nested_vector_unary "unit_vector_constrain" SoA;
+  add_nested_vector_unary "unit_vector_unconstrain" AoS;
   add_first_arg_vector_binary "upper_bound_jacobian" SoA;
   add_first_arg_vector_binary "upper_bound_constrain" SoA;
   add_first_arg_vector_binary "upper_bound_unconstrain" AoS;
