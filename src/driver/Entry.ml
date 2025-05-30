dde4fe04303c782ea3d9f719fdfa5fbcb7fb9eca
Brian Ward <bward@flatironinstitute.org>
2025-05-30 14:43:37 -0400

    Remove extraneous let

--- src/driver/Entry.ml
+++ src/driver/Entry.ml
@@ -65,80 +65,77 @@ let stan2cpp model_name model (flags : Flags.t) (output : other_output -> unit)
     if flags.functions_only then Parse.parse_stanfunctions model
     else Parse.parse_program model in
   output (Warnings parser_warnings);
-  let* result =
-    let* ast = ast in
-    if flags.debug_settings.print_ast then
-      output (DebugOutput (fmt_sexp [%sexp (ast : Ast.untyped_program)]));
-    let+ typed_ast, type_warnings =
-      Typechecker.check_program ast
-      |> Result.map_error ~f:(fun e -> Errors.Semantic_error e) in
-    if flags.debug_settings.print_typed_ast then
-      output (DebugOutput (fmt_sexp [%sexp (typed_ast : Ast.typed_program)]));
-    output (Warnings type_warnings);
-    if flags.info then output (Info (Info.info typed_ast));
-    let deprecation_warnings =
-      if flags.canonicalizer_settings.deprecations then []
-      else Deprecation_analysis.collect_warnings typed_ast in
-    output (Warnings deprecation_warnings);
-    if flags.auto_format then
-      output
-        (Formatted
-           (Pretty_print_prog.pretty_print_typed_program
-              ~bare_functions:flags.functions_only
-              ~line_length:flags.line_length
-              ~inline_includes:flags.canonicalizer_settings.inline_includes
-              ~strip_comments:flags.canonicalizer_settings.strip_comments
-              (Canonicalize.canonicalize_program typed_ast
-                 flags.canonicalizer_settings)));
-    let mir = Ast_to_Mir.trans_prog model_name typed_ast in
-    if flags.warn_uninitialized then
-      output (Warnings (Pedantic_analysis.warn_uninitialized mir));
-    if flags.warn_pedantic then
-      output (Warnings (Pedantic_analysis.warn_pedantic mir));
-    debug_output_mir output mir flags.debug_settings.print_mir;
-    let* generation_context =
-      match flags.debug_settings.debug_data_json with
-      | None -> Ok Map.Poly.empty
-      | Some string -> (
-          try
-            Ok
-              (Debug_data_generation.json_to_mir
-                 (Ast_to_Mir.gather_declarations typed_ast.datablock)
-                 (Yojson.Basic.from_string string))
-          with Yojson.Json_error reason ->
-            Error
-              (Errors.DebugDataError
-                 ( Location_span.empty
-                 , "Failed to parse data JSON for debug generation: " ^ reason
-                 ))) in
-    let* () =
-      if flags.debug_settings.debug_generate_data then
-        let+ data =
-          Debug_data_generation.gen_values_json ~context:generation_context
-            (Ast_to_Mir.gather_declarations typed_ast.datablock) in
-        output (Generated data)
-      else Ok () in
-    let+ () =
-      if flags.debug_settings.debug_generate_inits then
-        let+ inits =
-          Debug_data_generation.gen_values_json ~context:generation_context
-            (Ast_to_Mir.gather_declarations typed_ast.parametersblock) in
-        output (Generated inits)
-      else Ok () in
-    let tx_mir = Transform_Mir.trans_prog mir in
-    debug_output_mir output tx_mir flags.debug_settings.print_transformed_mir;
-    let opt_mir =
-      Optimize.optimization_suite
-        ~settings:(Flags.get_optimization_settings flags)
-        tx_mir in
-    if flags.debug_settings.print_mem_patterns then
-      output
-        (Memory_patterns (Fmt.str "%a" Memory_patterns.pp_mem_patterns opt_mir));
-    debug_output_mir output opt_mir flags.debug_settings.print_optimized_mir;
-    let cpp =
-      Lower_program.lower_program ?printed_filename:flags.filename_in_msg
-        opt_mir in
-    if flags.debug_settings.print_lir then
-      output (DebugOutput (fmt_sexp [%sexp (cpp : Cpp.program)]));
-    Fmt.(to_to_string Cpp.Printing.pp_program) cpp in
-  result
+  let* ast = ast in
+  if flags.debug_settings.print_ast then
+    output (DebugOutput (fmt_sexp [%sexp (ast : Ast.untyped_program)]));
+  let* typed_ast, type_warnings =
+    Typechecker.check_program ast
+    |> Result.map_error ~f:(fun e -> Errors.Semantic_error e) in
+  if flags.debug_settings.print_typed_ast then
+    output (DebugOutput (fmt_sexp [%sexp (typed_ast : Ast.typed_program)]));
+  output (Warnings type_warnings);
+  if flags.info then output (Info (Info.info typed_ast));
+  let deprecation_warnings =
+    if flags.canonicalizer_settings.deprecations then []
+    else Deprecation_analysis.collect_warnings typed_ast in
+  output (Warnings deprecation_warnings);
+  if flags.auto_format then
+    output
+      (Formatted
+         (Pretty_print_prog.pretty_print_typed_program
+            ~bare_functions:flags.functions_only ~line_length:flags.line_length
+            ~inline_includes:flags.canonicalizer_settings.inline_includes
+            ~strip_comments:flags.canonicalizer_settings.strip_comments
+            (Canonicalize.canonicalize_program typed_ast
+               flags.canonicalizer_settings)));
+  let mir = Ast_to_Mir.trans_prog model_name typed_ast in
+  if flags.warn_uninitialized then
+    output (Warnings (Pedantic_analysis.warn_uninitialized mir));
+  if flags.warn_pedantic then
+    output (Warnings (Pedantic_analysis.warn_pedantic mir));
+  debug_output_mir output mir flags.debug_settings.print_mir;
+  let* generation_context =
+    match flags.debug_settings.debug_data_json with
+    | None -> Ok Map.Poly.empty
+    | Some string -> (
+        try
+          Ok
+            (Debug_data_generation.json_to_mir
+               (Ast_to_Mir.gather_declarations typed_ast.datablock)
+               (Yojson.Basic.from_string string))
+        with Yojson.Json_error reason ->
+          Error
+            (Errors.DebugDataError
+               ( Location_span.empty
+               , "Failed to parse data JSON for debug generation: " ^ reason )))
+  in
+  let* () =
+    if flags.debug_settings.debug_generate_data then
+      let+ data =
+        Debug_data_generation.gen_values_json ~context:generation_context
+          (Ast_to_Mir.gather_declarations typed_ast.datablock) in
+      output (Generated data)
+    else Ok () in
+  let+ () =
+    if flags.debug_settings.debug_generate_inits then
+      let+ inits =
+        Debug_data_generation.gen_values_json ~context:generation_context
+          (Ast_to_Mir.gather_declarations typed_ast.parametersblock) in
+      output (Generated inits)
+    else Ok () in
+  let tx_mir = Transform_Mir.trans_prog mir in
+  debug_output_mir output tx_mir flags.debug_settings.print_transformed_mir;
+  let opt_mir =
+    Optimize.optimization_suite
+      ~settings:(Flags.get_optimization_settings flags)
+      tx_mir in
+  if flags.debug_settings.print_mem_patterns then
+    output
+      (Memory_patterns (Fmt.str "%a" Memory_patterns.pp_mem_patterns opt_mir));
+  debug_output_mir output opt_mir flags.debug_settings.print_optimized_mir;
+  let cpp =
+    Lower_program.lower_program ?printed_filename:flags.filename_in_msg opt_mir
+  in
+  if flags.debug_settings.print_lir then
+    output (DebugOutput (fmt_sexp [%sexp (cpp : Cpp.program)]));
+  Fmt.(to_to_string Cpp.Printing.pp_program) cpp
