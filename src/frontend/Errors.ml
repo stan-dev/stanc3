0113cff367b3fe8c95d8a33ab0e2ce66096b0cd5
Brian Ward <bward@flatironinstitute.org>
2025-06-18 09:55:08 -0400

    More styles, explanatory comment

--- src/frontend/Errors.ml
+++ src/frontend/Errors.ml
@@ -49,25 +49,54 @@ let pp_semantic_error ?printed_filename ?code ppf err =
     (Middle.Location_span.pp ?printed_filename)
     loc_span (pp_context ?code) loc_span.begin_loc Semantic_error.pp err
 
-let markup_text ?(text = Fmt.string) () ppf s =
-  let style_of_string = function
-    | "b" -> Some `Bold
-    | "i" -> Some `Italic
-    | "u" -> Some `Underline
-    | "f" -> Some `Faint
-    | "red" -> Some (`Fg `Red)
-    | "blue" -> Some (`Fg `Blue)
-    | "reset" -> Some `None
-    (* TODO: more *)
-    | _ -> None in
+let style_of_string : string -> Fmt.style option = function
+  | "b" | "bold" -> Some `Bold
+  | "i" -> Some `Italic
+  | "u" -> Some `Underline
+  | "f" | "faint" -> Some `Faint
+  | "r" | "reset" -> Some `None
+  | "reverse" -> Some `Reverse
+  | "black" -> Some (`Fg `Black)
+  | "cyan" -> Some (`Fg `Cyan)
+  | "green" -> Some (`Fg `Green)
+  | "white" -> Some (`Fg `White)
+  | "yellow" -> Some (`Fg `Yellow)
+  | "red" -> Some (`Fg `Red)
+  | "blue" -> Some (`Fg `Blue)
+  | "magenta" -> Some (`Fg `Magenta)
+  | "bg_black" -> Some (`Bg `Black)
+  | "bg_cyan" -> Some (`Bg `Cyan)
+  | "bg_green" -> Some (`Bg `Green)
+  | "bg_white" -> Some (`Bg `White)
+  | "bg_yellow" -> Some (`Bg `Yellow)
+  | "bg_red" -> Some (`Bg `Red)
+  | "bg_blue" -> Some (`Bg `Blue)
+  | "bg_magenta" -> Some (`Bg `Magenta)
+  | "light_black" -> Some (`Fg (`Hi `Black))
+  | "light_cyan" -> Some (`Fg (`Hi `Cyan))
+  | "light_green" -> Some (`Fg (`Hi `Green))
+  | "light_white" -> Some (`Fg (`Hi `White))
+  | "light_yellow" -> Some (`Fg (`Hi `Yellow))
+  | "light_red" -> Some (`Fg (`Hi `Red))
+  | "light_blue" -> Some (`Fg (`Hi `Blue))
+  | "light_magenta" -> Some (`Fg (`Hi `Magenta))
+  | _ -> None
+
+(** This function formats strings that contain styling directives using the
+  [Fmt.styled] system.
+  For example, the string ["$(red,Hello) $(b,world)!"] will be formatted
+  with "Hello" in red and "world" in bold, followed by an
+  exclamation mark. *)
+let markup_text ppf s =
   let max_i = String.length s - 1 in
   let buf = Buffer.create (String.length s) in
   let styles = Stack.create () in
   let pp_buffered () =
-    let styled =
-      Stack.fold ~f:(fun pp style -> Fmt.styled style pp) ~init:text styles
-    in
-    styled ppf (Buffer.contents buf);
+    let with_style =
+      Stack.fold
+        ~f:(fun pp style -> Fmt.styled style pp)
+        ~init:Fmt.string styles in
+    with_style ppf (Buffer.contents buf);
     Buffer.clear buf in
   let rec loop i =
     if i > max_i then pp_buffered ()
@@ -121,7 +150,7 @@ let pp_syntax_error ?printed_filename ?code ppf = function
   | Parsing (message, loc_span) ->
       Fmt.pf ppf "%a in %a, parsing error:@,%a@,%a" red "Syntax error"
         (Middle.Location_span.pp ?printed_filename)
-        loc_span (pp_context ?code) loc_span.begin_loc (markup_text ()) message
+        loc_span (pp_context ?code) loc_span.begin_loc markup_text message
   | Lexing loc ->
       Fmt.pf ppf "%a in %a, lexing error:@,%a@,%s@." red "Syntax error"
         (Middle.Location.pp ?printed_filename ())
