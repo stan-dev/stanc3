a2b84dcc54ab1947668de2d2e9a892e8a490ccef
Brian Ward <bward@flatironinstitute.org>
2025-06-27 10:08:52 -0400

    Clean up some pretty-printer usage

--- src/frontend/Warnings.ml
+++ src/frontend/Warnings.ml
@@ -4,10 +4,11 @@ module Location = Middle.Location
 type t = Location_span.t * string
 
 let pp ?printed_filename ppf (span, message) =
-  let loc_str =
-    if span = Location_span.empty then ""
-    else " in " ^ Location.to_string ?printed_filename span.begin_loc in
-  Fmt.pf ppf "@[<hov 4>Warning%s: %a@]" loc_str Fmt.text message
+  let maybe_loc =
+    Fmt.if' (span <> Location_span.empty) (fun ppf loc ->
+        Fmt.pf ppf " in %a" (Location.pp ?printed_filename ()) loc) in
+  Fmt.pf ppf "@[<hov 4>Warning%a: %a@]" maybe_loc span.begin_loc Fmt.text
+    message
 
 let pp_warnings ?printed_filename ppf warnings =
   if List.length warnings > 0 then
