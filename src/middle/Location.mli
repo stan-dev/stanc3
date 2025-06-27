6fafdafa97cb43e24648b6c7a265dbba0acd7d92
Brian Ward <bward@flatironinstitute.org>
2025-06-27 10:08:52 -0400

    Clean up some pretty-printer usage

---
+++ src/middle/Location.mli
@@ -0,0 +1,19 @@
+(** Storing locations in the original source *)
+
+(** Source code locations *)
+type t = {filename: string; line_num: int; col_num: int; included_from: t option}
+[@@deriving sexp, hash]
+
+val compare : t -> t -> int
+val empty : t
+
+val pp :
+     ?printed_filename:string
+  -> ?print_file:bool
+  -> ?print_line:bool
+  -> unit
+  -> t Fmt.t
+
+val pp_context_for : (t * string Array.t) Fmt.t
+(* Prints the text surrounding the provided [t].
+   If the callback fails, this gracefully degrades to printing nothing. *)
