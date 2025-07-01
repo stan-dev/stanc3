adb92e077510ede3cbe4818395a868d3923345df
Brian Ward <bward@flatironinstitute.org>
2025-07-01 15:58:04 -0400

    Remove some mutable state from Entry, prefer passing as flags

--- src/stan_math_backend/Transform_Mir.mli
+++ src/stan_math_backend/Transform_Mir.mli
@@ -4,6 +4,5 @@ the library implementation *)
 
 open Middle
 
-val trans_prog : Program.Typed.t -> Program.Typed.t
+val trans_prog : ?use_opencl:bool -> Program.Typed.t -> Program.Typed.t
 val is_opencl_var : string -> bool
-val use_opencl : bool ref
