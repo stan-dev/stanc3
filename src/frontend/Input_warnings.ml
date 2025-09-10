4a5a229b6f798c1703032071cd4b5a18febbb8e7
Brian Ward <brianmward99@gmail.com>
2025-09-10 11:42:27 -0400

    Fix empty model warning not respecting --filename-in-msg

--- src/frontend/Input_warnings.ml
+++ src/frontend/Input_warnings.ml
@@ -5,7 +5,7 @@ let init () = warnings := []
 let collect () = List.rev !warnings
 let add_warning span message = warnings := (span, message) :: !warnings
 
-let empty file =
-  add_warning Middle.Location_span.empty
-    ("Empty file '" ^ file
-   ^ "' detected; this is a valid stan model but likely unintended!")
+let empty () =
+  add_warning
+    (Preprocessor.current_location ())
+    "Empty model detected; this is a valid Stan model but likely unintended!"
