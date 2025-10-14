6d73efe95f5d184a8d01ede4ebf5c9b7b4710dda
Brian Ward <brianmward99@gmail.com>
2025-10-14 12:55:39 -0400

    Make sure core_kernel is available with build deps, not just dev deps
    Nonempty_list will be in the next Core version, c.f.
    https://github.com/janestreet/core/issues/177
    https://github.com/janestreet/core_kernel/commit/4b48de7e407e0881ba112e64aea507a939fc3eab

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -7,6 +7,6 @@ eval $(opam env)
 
 opam pin -y core v0.16.0 --no-action
 
-opam install -y dune core.v0.16.0 menhir.20230608 ppx_deriving.5.2.1 fmt.0.11.0 yojson.2.1.0 cmdliner.1.3.0
+opam install -y dune core.v0.16.0 core_kernel.v0.16.0 menhir.20230608 ppx_deriving.5.2.1 fmt.0.11.0 yojson.2.1.0 cmdliner.1.3.0
 
 eval $(opam env)
