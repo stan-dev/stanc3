fe5be0834b6816172056aaac4a7b946273fc5c13
Brian Ward <bward@flatironinstitute.org>
2026-07-06 10:08:13 -0400

    Format .mll and .mly files with menhirformat

--- scripts/install_ci_deps.sh
+++ scripts/install_ci_deps.sh
@@ -6,4 +6,4 @@ set -e
 # ocamlformat, bisect_ppx, and odoc are needed for CI and useful for developers
 opam pin -y ocamlformat 0.28.1 --no-action
 opam pin -y bisect_ppx https://github.com/aantron/bisect_ppx.git#7061d643ff492b0045796357ee6917ded21fb1f0 --no-action
-opam install -y ocamlformat bisect_ppx odoc sherlodoc
+opam install -y ocamlformat bisect_ppx odoc sherlodoc menhirformat
