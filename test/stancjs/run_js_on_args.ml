75ae9f240edef601a1177430ab1c2352bdcd7840
Brian Ward <bward@flatironinstitute.org>
2026-08-03 13:55:00 -0400

    Move over tests, remove Core stub

--- test/stancjs/run_js_on_args.ml
+++ test/stancjs/run_js_on_args.ml
@@ -1,18 +1,18 @@
-module Caml_unix = Unix
-open Core
+open Std
 
 let run_capturing_output cmd =
-  let env = [| "PATH=" ^ (Sys.getenv "PATH" |> Option.value ~default:"") |] in
-  let stdout, stdin, stderr = Caml_unix.open_process_full cmd env in
+  let env =
+    [| "PATH=" ^ (Sys.getenv_opt "PATH" |> Option.value ~default:"") |] in
+  let stdout, stdin, stderr = Unix.open_process_full cmd env in
   let chns = [stdout; stderr] in
   let out = List.map ~f:In_channel.input_lines chns in
-  ignore (Caml_unix.close_process_full (stdout, stdin, stderr));
+  ignore (Unix.close_process_full (stdout, stdin, stderr));
   String.concat ~sep:"\n" (List.concat out)
 
 let () =
-  let args = Sys.get_argv () in
+  let args = Sys.argv in
   let files = Array.(sub args ~pos:1 ~len:(length args - 1)) in
-  Array.stable_sort ~compare:String.compare files;
+  Array.stable_sort ~cmp:String.compare files;
   Array.iter files ~f:(fun arg ->
       let arg = String.chop_prefix_if_exists arg ~prefix:"./" in
       let cmd = "node " ^ arg in
