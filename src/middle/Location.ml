98c15cd3ce8e274f49faba861d77590f5a8f01a4
Brian Ward <bward@flatironinstitute.org>
2026-09-21 15:40:08 -0400

    Location tweaks

--- src/middle/Location.ml
+++ src/middle/Location.ml
@@ -24,10 +24,7 @@ let rec pp ?(print_file = true) ?(print_line = true) printed_filename ppf loc =
         , loc.filename )
     | None -> (ignore, Option.value ~default:loc.filename printed_filename)
   in
-  let file =
-    Fmt.if' print_file (fun ppf s ->
-        Fmt.pf ppf "%a, " Fmt.(styled (`Fg (`Hi `Blue)) (Fmt.fmt "'%s'")) s)
-  in
+  let file = Fmt.if' print_file (fun ppf s -> Fmt.pf ppf "'%s', " s) in
   let line = Fmt.if' print_line (Fmt.fmt "line %d, ") in
   Fmt.pf ppf "%a%acolumn %d%t" file filename line loc.line_num loc.col_num incl
 
@@ -40,11 +37,8 @@ let compare loc1 loc2 =
     | [], _ -> -1
     | _, [] -> 1
     | hd1 :: tl1, hd2 :: tl2 ->
-        let x = Int.compare hd1.line_num hd2.line_num in
-        if x <> 0 then x
-        else
-          let x = Int.compare hd1.col_num hd2.col_num in
-          if x <> 0 then x else go (tl1, tl2) in
+        let x = Int.compare hd1.byte_num hd2.byte_num in
+        if x <> 0 then x else go (tl1, tl2) in
   go (List.rev (unfold loc1), List.rev (unfold loc2))
 
 let rec initial_file_loc loc =
